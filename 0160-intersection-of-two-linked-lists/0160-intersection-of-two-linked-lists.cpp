c15dcfda4f5cc8c74ce3537032c93801efe95edc
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2024-09-08 09:55:45 +0530

    Time: 32 ms (87.48%), Space: 17.4 MB (25.11%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,41 +8,20 @@
  */
 class Solution {
 public:
-    ListNode* collisionPoint(ListNode *t1, ListNode *t2, int diff){
-        while(diff){
-            t2 = t2 -> next;
-            diff--;
-        }
+    // Best Optimal Solution that tends to bring the two pointers on same level
+    ListNode *getIntersectionNode(ListNode *head1, ListNode *head2) {   
+        ListNode* t1 = head1;
+        ListNode* t2 = head2;
 
         while(t1 != t2){
             t1 = t1 -> next;
             t2 = t2 -> next;
+
+            if(t1 == t2) return t1;
+
+            if(t1 == NULL) t1 = head2;
+            if(t2 == NULL) t2 = head1;
         }
         return t1;
-    }
-
-    // using 2 pointers to avoid space complexity 
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* t1 = headA;
-        ListNode* t2 = headB;
-
-        int count1 = 0;
-        int count2 = 0;
-
-        while(t1!=NULL){
-            count1++;
-            t1 = t1 -> next;
-        }
-
-        while(t2 != NULL){
-            count2++;
-            t2 = t2 -> next;
-        }
-
-        if(count1 < count2){
-            return collisionPoint(headA, headB, (count2-count1));
-        }else{
-            return collisionPoint(headB, headA, (count1-count2));
-        }
     }
 };
