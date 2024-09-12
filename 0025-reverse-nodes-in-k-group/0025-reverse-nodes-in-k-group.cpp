a356733f0a5d4cdc6158f0dc522b07bbd9509001
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2024-09-12 18:53:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,53 +10,55 @@
  */
 class Solution {
 public:
-    ListNode* getkthNode(ListNode* temp, int k){
+    ListNode* reverseLL(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* temp = head;
+
+        while(temp != NULL){
+            ListNode* next_node = temp -> next;
+            temp -> next = prev;
+            prev = temp;
+            temp = next_node;
+        }
+        return prev;
+    }
+
+    ListNode* getKthNode(ListNode* temp,int k){
         k--;
-        while(temp!= NULL && k > 0){
+        while(k > 0){
             k--;
             temp = temp -> next;
         }
         return temp;
     }
 
-    ListNode* reverseLL(ListNode* head){
-        ListNode* temp = head;
-        ListNode* prev = NULL;
-
-        while(temp != NULL){
-            ListNode* next = temp -> next;
-            temp -> next = prev;
-            prev = temp;
-            temp = next;
-        }
-        return prev;
-    }
-
     ListNode* reverseKGroup(ListNode* head, int k) {
         ListNode* temp = head;
-        ListNode* prevLast = NULL;
+        ListNode* prevNode = NULL;
 
         while(temp != NULL){
-            ListNode* kthnode = getkthNode(temp, k);
-            if(kthnode == NULL){
-                if(prevLast) prevLast -> next = temp;
-                break;
+            ListNode* kthNode = getKthNode(temp, k);
+            if(kthNode == NULL){
+                if(prevNode) {
+                    prevNode -> next = temp;
+                    break;
+                }
             }
 
-            ListNode* nextNode = kthnode -> next;
-            kthnode -> next = NULL;
+            ListNode* next_node = kthNode -> next;
+            kthNode -> next = NULL;
 
             reverseLL(temp);
-            if(temp == head){
-                head = kthnode;
+
+            if(head == temp){
+                head = kthNode;
             }else{
-                prevLast -> next = kthnode;
+                prevNode -> next = kthNode;
             }
 
-            prevLast = temp;
-            temp = nextNode;
+            prevNode = temp;
+            temp = next_node;
         }
         return head;
-        
     }
 };
