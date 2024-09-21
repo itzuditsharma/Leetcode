7cf19bd1c6bb6defa2c92ddc40f81a75a734cceb
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2024-09-21 18:05:14 +0530

    Time: 0 ms (100%), Space: 8.7 MB (26.63%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void recur(int i, string temp, string digits, unordered_map<char,string> &mapp, vector<string> &ans){
+        if(i == digits.size()){
+            ans.push_back(temp);
+            return;
+        }
+
+        string s = mapp[digits[i]];
+        for(int j = 0; j < s.size(); j++){
+            temp.push_back(s[j]);
+            recur(i+1, temp, digits, mapp, ans);
+            temp.pop_back();
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        
+        unordered_map<char, string> mapp;
+        mapp['2'] = "abc";
+        mapp['3'] = "def";
+        mapp['4'] = "ghi";
+        mapp['5'] = "jkl";
+        mapp['6'] = "mno";
+        mapp['7'] = "pqrs";
+        mapp['8'] = "tuv";
+        mapp['9'] = "wxyz";
+
+        vector<string> ans;
+        if(digits == "") return ans;
+        recur(0,"", digits, mapp, ans);
+        return ans;      
+    }
+};
