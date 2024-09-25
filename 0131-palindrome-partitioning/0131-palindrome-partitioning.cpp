f3f64b9d434446ceb3d9628c979f0ae552fd38c4
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2024-09-25 20:23:33 +0530

    Time: 92 ms (64.69%), Space: 61.4 MB (49.13%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
 
-    bool isPalindrome(string s,int start, int end){
+    bool isValid(string s, int start, int end){
         while(start <= end){
-            if(s[start]!= s[end]){
+            if(s[start] != s[end]) {
                 return false;
             }
             start++;
@@ -12,15 +12,15 @@ public:
         return true;
     }
 
-    void recur(int ind, string s, vector<string>&path, vector<vector<string>>&res){
+    void recur(int ind, string s, vector<string> &path, vector<vector<string>>&res){
         if(ind == s.size()){
             res.push_back(path);
             return;
         }
 
         for(int i = ind; i < s.size(); i++){
-            if(isPalindrome(s, ind, i)){
-                path.push_back(s.substr(ind, i-ind+1));
+            if(isValid(s, ind, i)){
+                path.push_back(s.substr(ind, i - ind + 1));
                 recur(i+1, s, path, res);
                 path.pop_back();
             }
