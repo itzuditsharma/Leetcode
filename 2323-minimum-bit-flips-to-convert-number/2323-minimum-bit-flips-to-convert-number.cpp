e410b07afd514899703d47702e86251e9bcf45ce
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2024-10-03 18:23:44 +0530

    Time: 0 ms (100%), Space: 7.6 MB (41.85%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -4,6 +4,7 @@ public:
         int n = start ^ goal;
         
         int count = 0;
+        // Check count number of set bits in notebook 
         while(n != 0){
             n = n & (n-1);
             count++;
