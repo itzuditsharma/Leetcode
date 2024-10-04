63d8a00abff444d36777b3d7be5b6feda1e04c6e
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2024-10-04 19:44:39 +0530

    Time: 0 ms (100%), Space: 8.6 MB (76.08%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,18 +3,17 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> ans;
-        int nosubsets = 1 << n;  //2^n
+        int nosubsets = (1<<n);
+
         for(int num = 0; num < nosubsets; num++){
             vector<int> ds;
-            for(int i = 0;i < n; i++){
-                // check if ith bit is set or not
-                if(num & (1 << i)){
+            for(int i = 0; i < n; i++){
+                if(num & (1<<i)){
                     ds.push_back(nums[i]);
                 }
             }
             ans.push_back(ds);
         }
-
         return ans;
     }
 };
