f5e8921b7dedcc1034e9b96a3454b35475ad4fde
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2024-10-05 12:19:37 +0530

    Time: 4 ms (22.77%), Space: 8.3 MB (72.99%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     // Can be found in Strivers Bit manipulation series 
-
+    
     double myPow(double x, int n) {
         double ans = 1.0;
         long long nn = n;
         if(n < 0) nn = -1*nn;
         while(nn > 0){
-            if(nn&1){
+            if(nn%2 == 1){
                 ans = ans * x;
                 nn = nn - 1;
             }else{
