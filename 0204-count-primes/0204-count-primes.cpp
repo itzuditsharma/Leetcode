649fb46655be630825b329ce9e767366875ae048
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2024-10-08 18:49:04 +0530

    Time: 138 ms (84.23%), Space: 132 MB (6.95%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        // Sieve of erathosthenes 
-
-        vector<int> nums(n, 1);
+        vector<int> primes(n, 1);
 
         for(int i = 2; i*i <= n; i++){
-            if(nums[i] == 1){
-                for(int j=i*i; j<n; j+=i){
-                    nums[j] = 0;
+            if(primes[i] == 1){
+                for(int j = i*i;j < n; j+=i){
+                    primes[j] = 0;
                 }
             }
         }
-
         int count = 0;
-        for(int i = 2; i < n; i++){
-            if(nums[i] == 1) count++;
+        for(int i = 2; i <n; i++){
+            if(primes[i] == 1) count++;
         }
-
         return count;
     }
 };
