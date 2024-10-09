78eb5a4568b32cb1f08eed7c38cd77b966e9a7be
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2024-10-09 14:58:29 +0530

    Time: 4 ms (70.86%), Space: 12.4 MB (24.84%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -4,14 +4,14 @@ public:
         unordered_map<int, int> mapp;
         stack<int> st;
 
-        int n1 = nums1.size();
         int n2 = nums2.size();
+        int n1 = nums1.size();
 
-        st.push(nums2[n2-1]);
         mapp[nums2[n2-1]] = -1;
+        st.push(nums2[n2-1]);
 
-        for(int i = n2-2; i>=0; i--){
-            while(!st.empty() && nums2[i] >= st.top()){
+        for(int i = n2-2; i >=0; i--){
+            while(!st.empty() && st.top()<=nums2[i]){
                 st.pop();
             }
 
@@ -20,8 +20,10 @@ public:
             }else{
                 mapp[nums2[i]] = st.top();
             }
+
             st.push(nums2[i]);
         }
+
         vector<int> ans;
         for(int i = 0; i < n1; i++){
             ans.push_back(mapp[nums1[i]]);
