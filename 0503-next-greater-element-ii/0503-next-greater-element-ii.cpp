7fa6346b1b8e249ecd04b7115576268243f4203e
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2024-10-10 16:10:17 +0530

    Time: 19 ms (81.24%), Space: 26.9 MB (30.49%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -5,8 +5,8 @@ public:
         vector<int> ans(n);
         stack<int> st;
 
-        for(int i = 2*n-1; i>=0; i--){
-            while(!st.empty() && st.top()<=nums[i%n]){
+        for(int i = 2*n-1; i >=0; i--){
+            while(!st.empty() && nums[i%n] >= st.top()){
                 st.pop();
             }
 
@@ -17,11 +17,8 @@ public:
                     ans[i] = st.top();
                 }
             }
-
-            st.push(nums[i%n]); 
+            st.push(nums[i%n]);
         }
-
         return ans;
-
     }
 };
