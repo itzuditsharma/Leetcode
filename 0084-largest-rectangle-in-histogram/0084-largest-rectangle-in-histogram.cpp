1cde978ceea58fa66461935d403e21df6c0a055d
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2024-10-11 19:36:15 +0530

    Time: 107 ms (67.47%), Space: 80 MB (74.13%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,55 +1,29 @@
 class Solution {
 public:
-    vector<int>NSL(vector<int> nums){
-        vector<int> ans(nums.size());
+    int largestRectangleArea(vector<int>& nums) {
         stack<int> st;
-
-        for(int i = 0; i < nums.size();i++){
-            while(!st.empty() && nums[st.top()] >= nums[i]){
-                st.pop();
-            }
-
-            if(st.empty()){
-                ans[i] = -1;
-            }else{
-                ans[i] = st.top();
-            }
-            st.push(i);
-        }
-        return ans;
-    }
-
-    vector<int>NSR(vector<int> nums){
-        vector<int> ans(nums.size());
-        stack<int> st;
-
-        for(int i = nums.size()-1; i >=0 ;i--){
-            while(!st.empty() && nums[st.top()] >= nums[i]){
-                st.pop();
-            }
-
-            if(st.empty()){
-                ans[i] = nums.size();
-            }else{
-                ans[i] = st.top();
-            }
-            st.push(i);
-        }
-        return ans;
-    }
-
-    int largestRectangleArea(vector<int>& heights) {
-        vector<int> nsl = NSL(heights);
-        vector<int> nsr = NSR(heights);
-
-
         int maxx = 0;
-        for(int i = 0; i < heights.size(); i++){
-            int l = nsl[i];
-            int r = nsr[i];
-            maxx = max(maxx, heights[i] * (r-l-1));
+
+        for(int i = 0;i < nums.size(); i++){
+            while(!st.empty() && nums[st.top()] > nums[i]){
+                int element = nums[st.top()];
+                st.pop();
+                int pse = (st.empty())? -1 : st.top();
+                int nse = i;
+
+                maxx = max(maxx, element *(nse - pse - 1));
+            }
+            st.push(i);
         }
-        return maxx;    
-        
+
+        while(!st.empty()){
+            int element = nums[st.top()];
+            st.pop();
+            int nse = nums.size();
+            int pse = (st.empty())? -1 : st.top();
+
+            maxx = max(maxx, element * (nse - pse - 1));
+        }
+        return maxx;
     }
 };
