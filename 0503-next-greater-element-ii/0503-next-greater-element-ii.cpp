51311b93f0c4a0a59e5847e6c6b4a4c743907280
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2024-10-17 16:23:33 +0530

    Time: 23 ms (56.33%), Space: 26.7 MB (75.08%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans(nums.size());
+        vector<int> ans(n);
         stack<int> st;
 
         for(int i = 2*n-1; i >=0; i--){
