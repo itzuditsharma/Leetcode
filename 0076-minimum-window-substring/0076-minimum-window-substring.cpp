0fbadf635518564c198387d1bd46f2c1f3e3b14f
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2024-12-03 16:30:28 +0530

    Time: 0 ms (100%), Space: 11.8 MB (8.05%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -3,33 +3,36 @@ public:
     string minWindow(string s, string t) {
         int l = 0;
         int r = 0;
-        int minlen = INT_MAX;
         int s_index = -1;
+        int minlen = INT_MAX;
+        int hash[256] = {0};
+        int count = 0;
+
         int n = s.size();
         int m = t.size();
-        int count = 0;
-        int hash[256] = {0};
-        for(int i = 0; i < t.size(); i++){
+
+        for(int i = 0; i < m; i++){
             hash[t[i]]++;
         }
 
         while(r < n){
-            if(hash[s[r]] > 0) count++;
+            if(hash[s[r]] > 0)count++;
             hash[s[r]]--;
-            
+
             while(count == m){
                 if(r-l+1 < minlen){
                     minlen = r-l+1;
                     s_index = l;
                 }
-
                 hash[s[l]]++;
-                if(hash[s[l]] > 0) count--;
+                if(hash[s[l]] > 0){
+                    count--;
+                }
                 l++;
             }
             r++;
         }
-        if(s_index == - 1) return "";
+        if(s_index == -1 ) return "";
         else return s.substr(s_index, minlen);
     }
 };
