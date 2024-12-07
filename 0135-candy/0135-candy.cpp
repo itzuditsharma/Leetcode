5f6349352cd511df466fb999e5ecff7e464c3f4a
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2024-12-07 20:05:05 +0530

    Time: 0 ms (100%), Space: 20.9 MB (28.21%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
+    // Slope method - Most optimal
     int candy(vector<int>& ratings) {
+        int i = 1;
         int n = ratings.size();
-        int left[n];
-        int right[n];
-        left[0] = 1;
-        right[n-1] = 1;
-
-        for(int i = 1; i < n; i++){
-            if(ratings[i] > ratings[i-1]){
-                left[i] = left[i-1] + 1;
-            }else{
-                left[i] = 1;
-            } 
-        }
-
-        int sum = max(left[n-1], 1);
-        int rit = 1; // (right)
-        int curr = 1;
-
-        for(int i = n-2; i >= 0; i--){
-            if(ratings[i] > ratings[i+1]){
-                curr = rit + 1;
-                rit = curr;
-            }else{
-                curr = 1;
-                rit = curr;
+        int sum = 1;
+        while(i < n){
+            if(ratings[i] == ratings[i-1]){
+                sum += 1;
+                i++;
+                continue;
             }
-            sum += max(left[i], curr);
-        }
+            int peak = 1;
+            while(i < n && ratings[i] > ratings[i-1]){
+                peak += 1;
+                sum += peak;
+                i++; 
+            }
 
+            int down = 1;
+            while(i < n && ratings[i] < ratings[i-1]){
+                sum += down;
+                down++;
+                i++;
+            }
+
+            if(down > peak){
+                sum += (down - peak);
+            }  
+        }
         return sum;
     }
 };
