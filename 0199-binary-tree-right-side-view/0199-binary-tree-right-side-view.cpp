1df09f9c9877fae618df1d5153650639ca901e2d
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2024-12-26 12:59:10 +0530

    Time: 0 ms (100%), Space: 15.2 MB (13.42%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,17 +11,6 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root, int level, vector<int> &res){
-        if(root == NULL) return;
-
-        if(res.size()==level){
-            res.push_back(root->val);
-        }
-
-        helper(root->right, level +1, res);
-        helper(root->left, level +1, res);
-    }
-
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
         if(root == NULL) return ans;
