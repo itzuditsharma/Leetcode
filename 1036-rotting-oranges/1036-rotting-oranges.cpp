e7e6e7dcb8270eb292c1769489b7eff4e8e3ecb0
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-01-15 15:56:44 +0530

    Time: 0 ms (100%), Space: 16.7 MB (74.3%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -3,8 +3,10 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
+        int time = 0;
         int vis[n][m];
-        queue<pair<pair<int, int>, int>> q;
+        queue<pair<pair<int, int>, int>>q;
+
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == 2){
@@ -12,33 +14,33 @@ public:
                     vis[i][j] = 2;
                 }else{
                     vis[i][j] = 0;
-                }
+                }   
             }
         }
-
-        int drow[] = {-1, 0, +1, 0};
-        int dcol[] = {0, +1, 0, -1};
-        int time = 0;
+        
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, 1, 0, -1};
 
         while(!q.empty()){
             int row = q.front().first.first;
             int col = q.front().first.second;
             int t = q.front().second;
-            time = max(time, t);
+            
             q.pop();
 
             for(int i = 0; i < 4; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
-                if(nrow >=0 && nrow < n && ncol >=0 && ncol < m && vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1){
-                    q.push({{nrow, ncol}, t+1});
+                if(nrow >=0 && nrow < n && ncol >=0 && ncol < m && grid[nrow][ncol] == 1 && vis[nrow][ncol] != 2){
                     vis[nrow][ncol] = 2;
+                    q.push({{nrow, ncol}, t + 1});
                 }
+                time = max(time, t);
             }
         }
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(vis[i][j] != 2 && grid[i][j] == 1){
+                if(grid[i][j] == 1 && vis[i][j] != 2){
                     return -1;
                 }
             }
