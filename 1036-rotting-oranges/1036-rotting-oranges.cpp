3453f697060d4f666be2598df355e918e4efe538
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-01-16 14:02:01 +0530

    Time: 0 ms (100%), Space: 16.7 MB (60.54%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -3,34 +3,33 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int time = 0;
-        int vis[n][m];
         queue<pair<pair<int, int>, int>>q;
+        int vis[n][m];
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == 2){
-                    q.push({{i, j}, 0});
+                    q.push({{i,j}, 0});
                     vis[i][j] = 2;
                 }else{
                     vis[i][j] = 0;
-                }   
+                }
             }
-        }
-        
-        int drow[] = {-1, 0, 1, 0};
+        }   
+        int time = 0;
+        int drow[] = {-1, 0, +1, 0};
         int dcol[] = {0, 1, 0, -1};
 
         while(!q.empty()){
             int row = q.front().first.first;
             int col = q.front().first.second;
             int t = q.front().second;
-            
             q.pop();
 
             for(int i = 0; i < 4; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
+
                 if(nrow >=0 && nrow < n && ncol >=0 && ncol < m && grid[nrow][ncol] == 1 && vis[nrow][ncol] != 2){
                     vis[nrow][ncol] = 2;
                     q.push({{nrow, ncol}, t + 1});
@@ -38,11 +37,10 @@ public:
                 time = max(time, t);
             }
         }
-        for(int i = 0; i < n; i++){
+
+        for(int i = 0;  i < n; i++){
             for(int j = 0; j < m; j++){
-                if(grid[i][j] == 1 && vis[i][j] != 2){
-                    return -1;
-                }
+                if(grid[i][j] == 1 && vis[i][j] != 2) return -1;
             }
         }
         return time;
