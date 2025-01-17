f652f3ed10ad3181e94e4b419020d85ce1935a78
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-01-17 18:03:32 +0530

    Time: 1 ms (54.03%), Space: 20.8 MB (30.35%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    void dfs(int node, vector<int> adjls[], vector<int>& vis){
+    void dfs(int node, vector<int> &vis, vector<int> adjls[]){
         vis[node] = 1;
+
         for(auto it : adjls[node]){
             if(!vis[it]){
-                dfs(it, adjls, vis);
+                dfs(it, vis, adjls);
             }
         }
     }
@@ -13,7 +14,7 @@ public:
         int n = isConnected.size();
         vector<int> adjls[n];
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i< n; i++){
             for(int j = 0; j < n; j++){
                 if(isConnected[i][j] == 1 && i != j){
                     adjls[i].push_back(j);
@@ -22,18 +23,15 @@ public:
             }
         }
 
-        vector<int> vis;
-        for(int i = 0; i < n; i++){
-            vis.push_back(0);
-        }
-
+        vector<int> vis(n, 0);
         int count = 0;
-        for(int i = 0; i < n; i++){
+        for(int i = 0;i < n; i++){
             if(!vis[i]){
                 count++;
-                dfs(i, adjls, vis);
+                dfs(i, vis, adjls);
             }
         }
+
         return count;
     }
 };
