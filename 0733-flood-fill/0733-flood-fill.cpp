80cc9f974e7fc28e2a7f36c332c21977f8372b7d
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-01-17 18:23:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    void dfs(int row, int col, int initial_color, int color, vector<vector<int>>& image,  vector<vector<int>> &new_image, int drow[], int dcol[]){
-        image[row][col] = color;
-
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        vector<vector<int>> new_image = image;
+        int ini_color = image[sr][sc];
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
         int n = image.size();
         int m = image[0].size();
 
-        for(int i = 0;i < 4; i++){
-            int nrow = row + drow[i];
-            int ncol = col + dcol[i];
-
-            if(nrow >=0 & nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == initial_color && new_image[nrow][ncol] != color){
-                dfs(nrow, ncol, initial_color, color, image, new_image, drow, dcol);
-            }
-        }
-    }
-
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int initial_color = image[sr][sc];
-        vector<vector<int>> new_image = image;
         int drow[] = {-1, 0, 1, 0};
         int dcol[] = {0, 1, 0, -1};
 
-        dfs(sr, sc, initial_color, color, image, new_image, drow, dcol);
-        return image;
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            for(int i=0;i < 4; i++){
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+
+                if(nrow >=0 && nrow < n && ncol >=0 && ncol < m && image[nrow][ncol] == ini_color && new_image[nrow][ncol] != color){
+                    q.push({nrow, ncol});
+                    new_image[nrow][ncol] = color;
+                }
+            }
+        }
+        return new_image;
     }
 };
