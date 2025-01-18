2c955ebc74b59bed34b38be12aa19ec78c72abbb
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-01-18 11:51:31 +0530

    Time: 28 ms (52.75%), Space: 24.2 MB (22.7%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
-    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>>& grid, int drow[], int dcol[]){
-        vis[row][col] = 1;
+    void bfs(int r, int c, vector<vector<int>> &vis, vector<vector<char>>& grid, int drow[], int dcol[]){
+        queue<pair<int, int>>q;
+        vis[r][c] = 1;
+        q.push({r, c});
         int n = grid.size();
         int m = grid[0].size();
 
-        for(int i = 0;i < 4; i++){
-            int nrow = row + drow[i];
-            int ncol = col + dcol[i];
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
 
-            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol] && grid[nrow][ncol] == '1'){
-                dfs(nrow, ncol, vis, grid, drow, dcol);
+            for(int i = 0; i < 4; i++){
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+
+                if(nrow >= 0 && nrow < n && ncol >=0 && ncol < m && !vis[nrow][ncol] && grid[nrow][ncol] == '1'){
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
+                }
             }
         }
     }
@@ -20,14 +29,15 @@ public:
         int m = grid[0].size();
         vector<vector<int>> vis(n, vector<int>(m, 0));
         int count = 0;
-        int dcol[] = {0, 1, 0, -1};
+
         int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, 1, 0, -1};
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(!vis[i][j] && grid[i][j] == '1'){
+                    bfs(i,j,vis, grid, drow, dcol);
                     count++;
-                    dfs(i, j, vis, grid, drow, dcol);
                 }
             }
         }
