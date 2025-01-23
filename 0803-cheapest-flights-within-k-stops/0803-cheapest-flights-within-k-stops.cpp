ea9cf4fd95cc02a87957f9561b10e5fbfb9b06d9
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-01-23 13:49:29 +0530

    Time: 6 ms (32.6%), Space: 18.4 MB (33.17%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -2,36 +2,38 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<vector<pair<int, int>>> adj(n);
-        // {node, edW} -> Edge weight
+
+        // node -> {node, edge weight }
         for(auto it : flights){
             adj[it[0]].push_back({it[1], it[2]});
         }
 
-        // {stops, {node, distance}}
-        queue<pair<int, pair<int, int>>>q;
-        q.push({0, {src,0}});
-
         vector<int> dist(n, 1e9);
         dist[src] = 0;
+        
+        // {stops, {node, distance}}
+        queue<pair<int, pair<int, int>>> q;
+        q.push({0, {src, 0}});
 
         while(!q.empty()){
             auto it = q.front();
-            q.pop();
-
             int stops = it.first;
             int node = it.second.first;
             int cost = it.second.second;
 
+            q.pop();
+
             for(auto it : adj[node]){
                 int adjNode = it.first;
-                int edW = it.second;
+                int edw = it.second;
 
-                if(dist[adjNode] > edW + cost && stops <= k){
-                    dist[adjNode] = edW + cost;
-                    q.push({stops+1, {adjNode, edW + cost}});
+                if(dist[adjNode] > edw + cost && stops <= k){
+                    dist[adjNode] = edw + cost;
+                    q.push({stops+1, {adjNode, edw + cost}});
                 }
             }
         }
+
         if(dist[dst] == 1e9) return -1;
         return dist[dst];
     }
