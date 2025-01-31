869a7ded1bcafc16774e79e8342c00ab45d62084
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-01-31 15:14:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,78 @@
+class DisjointSet{
+public:
+    vector<int> rank, parent, size;
+    // Constructor
+    DisjointSet(int n){
+        rank.resize(n+1, 0);
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }    
+    // Find ultimate parent of a node
+    int findUPar(int node){
+        if(node == parent[node]){
+            return node;
+        }
+        return parent[node] = findUPar(parent[node]);
+    }
+    void unionByRank(int u, int v){
+        int ultp_u = findUPar(u);
+        int ultp_v = findUPar(v);
+        if(ultp_u == ultp_v) return;
+        if(rank[ultp_u] < rank[ultp_v]){
+            parent[ultp_u] = ultp_v;
+        }else if(rank[ultp_v] < rank[ultp_u]){
+            parent[ultp_v] = ultp_u;
+        }else{
+            parent[ultp_v] = ultp_u;
+            rank[ultp_u]++;
+        }
+    }
+    
+    void unionBySize(int u, int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        
+        if(ulp_u ==ulp_v) return;
+        
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        int maxRow = 0;
+        int maxCol = 0;
+
+        for(auto it : stones){
+            maxRow = max(maxRow, it[0]);
+            maxCol = max(maxCol, it[1]);
+        }
+
+        DisjointSet ds(maxRow + maxCol + 1);
+
+        for(auto it : stones){
+            int nodeRow = it[0];
+            int nodeCol = it[1] + maxRow + 1;
+
+            ds.unionBySize(nodeRow, nodeCol);
+        }
+
+        int count = 0;
+        for(int i = 0; i < maxRow + maxCol + 1; i++){
+            if(ds.parent[i] == i && ds.size[i] > 0) count++; 
+        }
+        return n - count;
+    }
+};
