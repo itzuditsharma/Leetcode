dd1e4903495db252d04f24264a833f7a799c9a20
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-02-01 18:08:58 +0530

    Time: 48 ms (72.51%), Space: 24.5 MB (53.23%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-
-        // pair -> {dis , {row, col}}
         priority_queue<pair<int, pair<int, int>>, 
         vector<pair<int, pair<int, int>>>, 
         greater<pair<int, pair<int, int>>>> pq;
         pq.push({0, {0,0}});
-
+        // diff , row , col 
         int n = heights.size();
         int m = heights[0].size();
 
@@ -16,14 +14,13 @@ public:
 
         int drow[] = {-1, 0, 1, 0};
         int dcol[] = {0, 1, 0, -1};
- 
+
         while(!pq.empty()){
             auto it = pq.top();
-            pq.pop();
-
             int diff = it.first;
             int row = it.second.first;
             int col = it.second.second;
+            pq.pop();
 
             if(row == n-1 && col == m-1) return diff;
 
@@ -31,9 +28,9 @@ public:
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
 
-                if(nrow >= 0 && nrow <n && ncol >=0 && ncol < m){
+                if(nrow >=0 && nrow < n && ncol >=0 && ncol < m){
                     int newDiff = max(diff, abs(heights[nrow][ncol] - heights[row][col]));
-                    if(newDiff < dist[nrow][ncol]){
+                    if(dist[nrow][ncol] > newDiff){
                         dist[nrow][ncol] = newDiff;
                         pq.push({newDiff, {nrow, ncol}});
                     }
