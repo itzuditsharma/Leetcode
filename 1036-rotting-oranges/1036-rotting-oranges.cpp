e4db3693a7df142bef5862fbc00bde0280dc40f9
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-02-04 11:25:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -3,8 +3,8 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        queue<pair<pair<int, int>, int>>q;
         int vis[n][m];
+        queue<pair<pair<int, int>, int>> q;
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
@@ -15,32 +15,37 @@ public:
                     vis[i][j] = 0;
                 }
             }
-        }   
-        int time = 0;
-        int drow[] = {-1, 0, +1, 0};
+        }
+
+        int drow[] = {-1, 0, 1, 0};
         int dcol[] = {0, 1, 0, -1};
 
+        int time = 0;
         while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            int t = q.front().second;
+            auto it = q.front();
+            int row = it.first.first;
+            int col = it.first.second;
+            int t = it.second;
             q.pop();
+
+            time = max(t, time);
 
             for(int i = 0; i < 4; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
 
-                if(nrow >=0 && nrow < n && ncol >=0 && ncol < m && grid[nrow][ncol] == 1 && vis[nrow][ncol] != 2){
+                if(nrow >= 0 && nrow < n && ncol >=0 && ncol < m && grid[nrow][ncol] == 1 && vis[nrow][ncol] != 2){
+                    q.push({{nrow, ncol}, t+1});
                     vis[nrow][ncol] = 2;
-                    q.push({{nrow, ncol}, t + 1});
                 }
-                time = max(time, t);
             }
         }
 
-        for(int i = 0;  i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 1 && vis[i][j] != 2) return -1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1 && vis[i][j] != 2){
+                    return {-1};
+                }
             }
         }
         return time;
