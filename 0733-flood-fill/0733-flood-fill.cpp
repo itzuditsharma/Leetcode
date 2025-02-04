e77e85558e161a1aa5260c183b6ec1773ea5ccc3
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-02-04 12:34:48 +0530

    Time: 0 ms (100%), Space: 18.2 MB (47.92%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -4,10 +4,10 @@ public:
         vector<vector<int>> new_image = image;
         int ini_color = image[sr][sc];
         new_image[sr][sc] = color;
-        queue<pair<int, int>> q;
-        q.push({sr, sc});
         int n = image.size();
         int m = image[0].size();
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
 
         int drow[] = {-1, 0, 1, 0};
         int dcol[] = {0, 1, 0, -1};
@@ -17,13 +17,13 @@ public:
             int col = q.front().second;
             q.pop();
 
-            for(int i=0;i < 4; i++){
+            for(int i = 0; i < 4; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
 
                 if(nrow >=0 && nrow < n && ncol >=0 && ncol < m && image[nrow][ncol] == ini_color && new_image[nrow][ncol] != color){
-                    q.push({nrow, ncol});
                     new_image[nrow][ncol] = color;
+                    q.push({nrow, ncol});
                 }
             }
         }
