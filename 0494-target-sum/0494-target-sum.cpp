6a4f0d5f4076689994d1f21a64cca8aeb495c534
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-02-16 13:32:17 +0530

    Time: 0 ms (100%), Space: 15.4 MB (46.1%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -24,7 +24,20 @@ public:
         if(totalsum - target < 0 || (totalsum - target) %2 ==1) return 0;
 
         int s2 = (totalsum - target)/2;
-        vector<vector<int>> dp(n, vector<int>(s2+1, -1));
-        return f(n-1, s2, nums, dp);
+        vector<vector<int>> dp(n, vector<int>(s2+1, 0));
+        if(nums[0] == 0) dp[0][0] = 2;
+        else dp[0][0] = 1;
+
+        if (nums[0] <= s2 && nums[0] != 0) dp[0][nums[0]] = 1;
+        for(int i = 1; i < n; i++){
+            for(int tar = 0; tar <= s2; tar++){
+                int nottake = dp[i - 1][tar];
+                int take = 0;
+                if(nums[i] <= tar) take = dp[i-1][tar - nums[i]];
+
+                dp[i][tar] = take + nottake;
+            }
+        }
+        return dp[n-1][s2];
     }
 };
