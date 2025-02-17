232f16813cb8f4c2fdb8a29b2759f356c27ede3d
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-02-17 09:50:21 +0530

    Time: 42 ms (42.04%), Space: 49.4 MB (28.02%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
+    int f(int ind, int T, vector<int>& coins, vector<vector<int>> &dp){
+        if(ind == 0){
+            if(T % coins[ind] == 0) return T / coins[ind];
+            return 1e9;
+        }
+        if(dp[ind][T] != -1) return dp[ind][T];
+
+        int nottake = f(ind-1, T, coins, dp);
+        int take = INT_MAX;
+        if(coins[ind] <= T) take = 1 + f(ind, T - coins[ind], coins, dp);
+
+        return dp[ind][T] = min(take, nottake);
+    }
 
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        
-        vector<int> prev(amount+1, 0);
-        vector<int> curr(amount+1, 0);
+        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        int ans = f(n-1, amount, coins, dp);
 
-        for(int T = 0; T <= amount; T++){
-            if(T % coins[0] == 0) prev[T] = T / coins[0];
-            else prev[T] = 1e9;
-        }
-
-        for(int i = 1; i < n; i++){
-            for(int t = 0; t <= amount; t++){
-                int nottake = prev[t];
-                int take = INT_MAX;
-                if(coins[i] <= t) take = 1 + curr[t-coins[i]];
-
-                curr[t] = min(take, nottake);
-            }
-            prev = curr;
-        }
-        if(prev[amount] >= 1e9) return -1;
-        else return prev[amount];
+        if(ans >= 1e9) return -1;
+        return ans;
     }
 };
