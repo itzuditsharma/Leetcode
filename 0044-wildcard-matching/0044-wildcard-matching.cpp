5de703433500f0e68b325a220ba4648bd54551cd
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-02-22 16:31:37 +0530

    Time: 34 ms (48.48%), Space: 31.2 MB (37.61%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     bool f(int i, int j, string &pattern, string &text, vector<vector<int>> &dp){
-        if(i < 0 && j < 0) return true;
-        if(i < 0 && j >=0 ) return false;
-        if(j < 0 && i >= 0){
-            for(int it = 0; it <=i; it++){
-                if(pattern[it]!='*') return false;
+        if(i == 0 && j == 0) return true;
+        if(i == 0 && j >0 ) return false;
+        if(j == 0 && i > 0){
+            for(int it = 1; it <=i; it++){
+                if(pattern[it-1]!='*') return false;
             }
             return true;
         }
         if(dp[i][j] != -1) return dp[i][j];
 
-        if(pattern[i] == text[j] || pattern[i] == '?'){
+        if(pattern[i-1] == text[j-1] || pattern[i-1] == '?'){
             return dp[i][j] = f(i-1, j-1, pattern, text, dp);
         }
 
-        if(pattern[i] == '*'){
+        if(pattern[i-1] == '*'){
             return dp[i][j] =  f(i-1,j, pattern, text, dp) | f(i, j-1, pattern, text, dp);
         }
 
@@ -25,7 +25,7 @@ public:
     bool isMatch(string s, string p) {
         int n = p.size();
         int m = s.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return f(n-1, m-1, p, s, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return f(n, m, p, s, dp);
     }
 };
