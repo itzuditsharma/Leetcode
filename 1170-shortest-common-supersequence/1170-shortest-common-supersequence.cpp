15869c787a2084bb3b2ea0f74d3e91bedb55bf7d
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-02-23 19:33:29 +0530

    Time: 34 ms (6.88%), Space: 26.8 MB (43.56%) - LeetHub

---
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int findLCS(int i, int j, string& s1, string &s2, vector<vector<int>> &dp){
+        if(i == 0 || j == 0) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s1[i-1] == s2[j-1]){
+            return dp[i][j] = 1 + findLCS(i-1, j-1, s1, s2, dp);
+        }
+
+        return dp[i][j] = max(findLCS(i-1, j, s1, s2, dp), findLCS(i, j-1, s1, s2, dp));
+    }
+
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n = str1.size();
+        int m = str2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        int lcs = findLCS(n, m, str1, str2, dp);
+
+        int i = n;
+        int j = m;
+        string ans = "";
+
+        while(i > 0 && j > 0){
+            if(str1[i-1] == str2[j-1]){
+                ans += str1[i-1];
+                i--;
+                j--;
+            }else if(dp[i-1][j] > dp[i][j-1]){
+                ans += str1[i-1];
+                i--;
+            }else{
+                ans += str2[j-1];
+                j--;
+            }
+        }
+
+        while(i > 0){
+            ans+=str1[i-1];
+            i--;
+        }
+
+        while(j > 0){
+            ans += str2[j-1];
+            j--;
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
