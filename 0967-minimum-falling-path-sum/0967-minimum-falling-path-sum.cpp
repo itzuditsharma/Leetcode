e9eae93594838283a1f1ae47aac19daaaf02fd25
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-03-14 09:02:27 +0530

    Time: 0 ms (100%), Space: 15 MB (60.14%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,49 +1,37 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp){
-        if(j < 0 || j >= matrix[0].size()) return 1e9;
-        if(i == 0) return matrix[0][j];
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int up = matrix[i][j] + f(i-1, j, matrix, dp);
-        int diag_left = matrix[i][j] + f(i-1, j-1, matrix, dp);
-        int diag_right = matrix[i][j] + f(i-1, j+1, matrix, dp);
-
-        return dp[i][j] = min(up, min(diag_left, diag_right));
-    }
-
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        int m = matrix[0].size();
-
-        vector<int> prev(m, 0);
-        
-        for(int j = 0; j < m; j++){
-            prev[j] = matrix[0][j];
+        vector<vector<int>>dp(n, vector<int>(n, 0));
+        for(int j = 0; j < n; j++){
+            dp[0][j] = matrix[0][j];
         }
 
-        for(int i = 1; i < n; i++){
-            vector<int> curr(m, 0);
-            for(int j = 0; j < m; j++){
-                int diag_left = 1e9;
-                int diag_right = 1e9;
+        for(int i = 1;  i< n; i++){
+            for(int j = 0; j < n; j++){
+                int up = matrix[i][j] + dp[i-1][j];
 
-                int up = matrix[i][j] + prev[j];
+                int diagonal_left = matrix[i][j];
+                if(j -1 >= 0){
+                    diagonal_left += dp[i-1][j-1];
+                }else{
+                    diagonal_left += 1e9;
+                }
 
-                if(j > 0 )diag_left = matrix[i][j] + prev[j-1];
-                if(j < m -1  )diag_right = matrix[i][j] + prev[j+1];
-
-                curr[j] = min(up, min(diag_left, diag_right));
+                int diagonal_right = matrix[i][j];
+                if(j +1 < n){
+                    diagonal_right += dp[i-1][j+1];
+                }else{
+                    diagonal_right += 1e9;
+                }
+                dp[i][j] = min(up, min(diagonal_left, diagonal_right));
             }
-            prev = curr;
-
         }
 
-        int min_sum = 1e9;
-        for(int j = 0; j < m; j++){
-            min_sum = min(min_sum, prev[j]);
+        int mini = 1e9;
+        for(int i = 0; i < n; i++){
+            mini = min(mini, dp[n-1][i]);
         }
-        return min_sum;
+        return mini;
     }
 };
