1958991e087d5d6526a997d8a39e29a0a02c03d5
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-03-29 00:25:18 +0530

    Time: 85 ms (43.33%), Space: 76 MB (42.1%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int LCS(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
-        if(i < 0 || j < 0) return 0;
+    int LCS(int i1, int i2, string &s1, string &s2, vector<vector<int>> &dp){
+        if(i1 == 0 || i2 == 0) return 0;
 
-        if(dp[i][j] != -1) return dp[i][j];
+        if(dp[i1][i2] != -1) return dp[i1][i2];
 
-        if(s1[i] == s2[j]) {
-            return dp[i][j] = 1 + LCS(i-1, j-1, s1, s2, dp);
+        if(s1[i1-1] == s2[i2-1]){
+            return dp[i1][i2] = 1 + LCS(i1 -1, i2 -1, s1, s2, dp);
         }
-        
-        return dp[i][j] = max(LCS(i-1, j, s1, s2, dp), LCS(i, j-1, s1, s2, dp));
+
+        return dp[i1][i2] = max(LCS(i1 -1, i2, s1, s2, dp), LCS(i1, i2-1, s1, s2, dp));
     }
 
     int longestPalindromeSubseq(string s) {
@@ -19,7 +19,7 @@ public:
         int n = s.size();
         int m = s2.size();
 
-        vector<vector<int>> dp(n, vector<int>(m+1, -1));
-        return LCS(n-1, m-1, s, s2, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return LCS(n, m, s, s2, dp);
     }
 };
