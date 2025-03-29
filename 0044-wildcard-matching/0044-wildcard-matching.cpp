de9aaa33139495303868c825db5031f88b173480
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-03-29 12:29:54 +0530

    Time: 38 ms (29.67%), Space: 30.9 MB (37.04%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    bool isMatch(string text, string pattern) {
-        int n = pattern.size();
-        int m = text.size();
-        vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
-        dp[0][0] = true;
-        for(int j=1;j<=m;j++){
-            dp[0][j] = false;
-        }
-        for(int i = 1; i <=n; i++){
-            bool flag = true;
-            for(int it = 1; it <=i; it++){
-                if(pattern[it-1]!='*'){
-                    flag =  false;
-                    break;
-                }
+    bool f(int i, int j, string &p, string &s, vector<vector<int>> &dp){
+        if(i < 0 && j < 0) return true;
+        if(i < 0 && j >=0 ) return false;
+        if(i >= 0 && j < 0){
+            for(int it = 0; it <= i; it++){
+                if(p[it] != '*') return false;
+                j--;
             }
-            dp[i][0] = flag;
+            return true;
         }
 
-        for(int i = 1; i <=n; i++){
-            for(int j = 1; j <=m; j++){
-                if(pattern[i-1] == text[j-1] || pattern[i-1] == '?'){
-                    dp[i][j] = dp[i-1][j-1];
-                }
 
-                else if(pattern[i-1] == '*'){
-                    dp[i][j] =  dp[i-1][j] | dp[i][j-1];
-                }
+        if(dp[i][j] != -1) return dp[i][j];
 
-                else dp[i][j] = false;
-            }
+        if(p[i] == s[j] || p[i] == '?'){
+            return dp[i][j] = f(i-1, j-1, p, s, dp);
         }
-        return dp[n][m];
+
+        if(p[i] == '*'){
+            return dp[i][j] = f(i-1, j, p, s, dp) | f(i, j-1, p, s, dp);
+        }
+        return dp[i][j] = false;
+    }
+
+    bool isMatch(string s, string p) {
+        int n = p.size();
+        int m = s.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return f(n-1, m-1, p, s, dp);
     }
 };
