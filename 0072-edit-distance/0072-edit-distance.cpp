820d583ee8242b5946d9774064e61f165ef77cee
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-03-29 11:38:23 +0530

    Time: 5 ms (72.06%), Space: 12.8 MB (80.33%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int f(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
+    int min_operations(int i, int j, string &word1, string &word2, vector<vector<int>> &dp){
         if(i < 0) return j+1;
         if(j < 0) return i+1;
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        
-        if(s1[i] == s2[j]){
-            return dp[i][j] = 0 + f(i-1, j-1, s1, s2, dp);
+        if(word1[i] == word2[j]){
+            return dp[i][j] = 0 + min_operations(i-1, j-1, word1, word2, dp);
         }
 
-        return dp[i][j] =  1 + min(f(i-1, j, s1, s2, dp), min(f(i, j-1, s1, s2, dp), f(i-1, j-1, s1, s2, dp)));
+        return dp[i][j] = 1 + min(min_operations(i, j-1, word1, word2, dp), min(min_operations(i-1, j, word1, word2, dp), min_operations(i-1, j-1, word1, word2, dp)));
+                                                // Insertion                                    //Deletion                              Replacement
     }
 
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
         vector<vector<int>> dp(n, vector<int>(m+1, -1));
-        return f(n-1, m-1, word1, word2, dp);
+        return min_operations(n-1, m-1, word1, word2, dp);
     }
 };
