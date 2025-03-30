b9dc72595a584d94f990ba5b3f2a9b07fee85371
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-03-30 12:48:15 +0530

    Time: 11 ms (67.29%), Space: 12.6 MB (46.49%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -5,23 +5,19 @@ public:
         sort(nums.begin(), nums.end());
         vector<int> dp(n, 1);
         vector<int> hash(n);
-
-        for(int i = 0; i < n; i++){
-            hash[i] = i;
-        }
-        
-        int maxi = 0;
+        int maxlen = 0;
         int last_index = 0;
+        for(int i = 0; i < n; i++) hash[i] = i;
 
         for(int i = 0; i < n; i++){
             for(int prev = 0; prev < i; prev++){
-                if(nums[i] % nums[prev] == 0 && dp[prev] + 1 > dp[i]){
-                    dp[i] = dp[prev] + 1;
+                if(nums[i] % nums[prev] == 0 && 1+dp[prev] > dp[i]){
+                    dp[i] = 1 + dp[prev];
                     hash[i] = prev;
                 }
             }
-            if(dp[i] > maxi){
-                maxi = dp[i];
+            if(maxlen < dp[i]){
+                maxlen = dp[i];
                 last_index = i;
             }
         }
@@ -32,7 +28,6 @@ public:
             last_index = hash[last_index];
             temp.push_back(nums[last_index]);
         }
-
         return temp;
     }
 };
