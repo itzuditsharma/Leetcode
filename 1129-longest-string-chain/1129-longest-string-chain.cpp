c21626dff2fb76b52241ab7dec728655b909196f
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-03-30 13:13:02 +0530

    Time: 43 ms (65.95%), Space: 18.9 MB (64.02%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,41 +1,49 @@
 class Solution {
 public:
-    bool checkPossible(string &s1, string &s2){
-        if(s1.size() != s2.size() + 1) return false;
+    bool possible(string &s1, string &s2){
+        if(s2.size()+1 != s1.size()) return false;
+        int i = 0;
+        int j = 0;
 
-        int first  = 0;
-        int second = 0;
-
-        while(first < s1.size()){
-            if(second < s2.size() && s1[first] == s2[second]){
-                first++;
-                second++;
+        while(i < s1.size()){
+            if(j < s2.size() && s1[i] == s2[j]){
+                i++;
+                j++;
             }else{
-                first++;
+                i++;
             }
         }
-        if(first == s1.size() && second == s2.size()) return true;
-        return false; 
+        if(i == s1.size() && j == s2.size()) return true;
+        return false;
     }
 
     static bool comp(string &s1, string &s2){
         return s1.size() < s2.size();
     }
 
-    int longestStrChain(vector<string>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end(), comp);
+    int longestStrChain(vector<string>& words) {
+        int n = words.size();
         vector<int> dp(n, 1);
-        int maxi = 0;
+        vector<int> hash(n);
+        for(int i = 0; i < n; i++){
+            hash[i] = i;
+        }
+        sort(words.begin(), words.end(), comp);
+        int last_index = 0;
+        int maxlen = 0;
 
         for(int i = 0; i < n; i++){
             for(int prev = 0; prev < i; prev++){
-                if(checkPossible(nums[i], nums[prev]) && 1 + dp[prev] > dp[i]){
+                if(possible(words[i], words[prev]) && 1 + dp[prev] > dp[i]){
                     dp[i] = 1 + dp[prev];
+                    hash[i] = prev;
                 }
             }
-            maxi = max(maxi, dp[i]);
+            if(maxlen < dp[i]){
+                maxlen = dp[i];
+                last_index = i;
+            }
         }
-        return maxi;
+        return maxlen;;
     }
 };
