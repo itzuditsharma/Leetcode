df45e1a65a1b200a2e18bbcfb461d3cf2579a5e2
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-04-01 21:45:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,8 +3,8 @@ public:
     int f(int n, vector<int> &dp){
         if(n < 0) return 0;
         if(n == 0) return 1;
-
-        return f(n-1, dp) + f(n-2, dp);
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = f(n-1, dp) + f(n-2, dp);
     }
 
     int climbStairs(int n) {
