c38d9351339a91ecc4357e134c15cb4e7ab3ecc0
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-04-01 23:47:50 +0530

    Time: 0 ms (100%), Space: 10.9 MB (21.35%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-
-    int f(int ind, vector<int> &nums, vector<int> &dp){
-        if(ind == 0) return nums[ind];
+    int f(int ind, vector<int>&nums, vector<int>&dp){
+        if(ind == 0) return nums[0];
         if(ind < 0) return 0;
+
         if(dp[ind] != -1) return dp[ind];
+        int take = f(ind -2, nums, dp) + nums[ind];
+        int nottake = f(ind - 1, nums, dp);
 
-        int take = nums[ind] + f(ind - 2, nums, dp);
-
-        int not_take = 0 + f(ind -1, nums, dp);
-
-        return dp[ind] = max(take, not_take);
+        return dp[ind] = max(take, nottake);
     }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return nums[0];
-        vector<int> temp1, temp2;
+        vector<int> one;
+        vector<int> two;
 
         for(int i = 0; i < n; i++){
-            if(i != 0) temp1.push_back(nums[i]);
-            if(i != n-1) temp2.push_back(nums[i]);
+            if(i != n-1) one.push_back(nums[i]);
+            if(i != 0) two.push_back(nums[i]);
         }
-        int n1 = temp1.size();
-        int n2 = temp2.size();
+
+        int n1 = one.size();
+        int n2 = two.size();
         vector<int> dp1(n1+1, -1);
         vector<int> dp2(n2+1, -1);
-        // return max(f(n-2, temp1,dp1), f(n-2, temp2,dp2));
-        return max(f(n1-1, temp1,dp1), f(n2-1, temp2,dp2));
+
+        return max(f(n1-1, one, dp1), f(n2-1, two, dp2));
     }
 };
