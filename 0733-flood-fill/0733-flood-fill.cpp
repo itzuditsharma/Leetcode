2f7c9fa9be70b630fe7fde71e6bae967675fbad6
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-04-02 21:44:36 +0530

    Time: 0 ms (100%), Space: 18.4 MB (23.32%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int initial_color = image[sr][sc];
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
         vector<vector<int>> new_image = image;
-        int ini_color = image[sr][sc];
         new_image[sr][sc] = color;
         int n = image.size();
         int m = image[0].size();
-        queue<pair<int, int>> q;
-        q.push({sr, sc});
 
-        int drow[] = {-1, 0, 1, 0};
+        int drow[]  = {-1, 0, 1, 0};
         int dcol[] = {0, 1, 0, -1};
 
         while(!q.empty()){
@@ -21,7 +21,7 @@ public:
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
 
-                if(nrow >=0 && nrow < n && ncol >=0 && ncol < m && image[nrow][ncol] == ini_color && new_image[nrow][ncol] != color){
+                if(nrow >=0 && nrow < n && ncol >=0 && ncol < m && new_image[nrow][ncol] != color && image[nrow][ncol] == initial_color){
                     new_image[nrow][ncol] = color;
                     q.push({nrow, ncol});
                 }
