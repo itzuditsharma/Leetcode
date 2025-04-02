d96a7eb1fc2621eaa981c4a6fab945bcc072880f
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-04-02 20:05:44 +0530

    Time: 0 ms (100%), Space: 16.7 MB (66.41%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -9,43 +9,40 @@ public:
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == 2){
-                    q.push({{i,j}, 0});
+                    q.push({{i, j}, 0});
                     vis[i][j] = 2;
                 }else{
                     vis[i][j] = 0;
                 }
             }
         }
-
+        
         int drow[] = {-1, 0, 1, 0};
         int dcol[] = {0, 1, 0, -1};
-
         int time = 0;
+
         while(!q.empty()){
             auto it = q.front();
             int row = it.first.first;
             int col = it.first.second;
             int t = it.second;
-            q.pop();
 
-            time = max(t, time);
+            q.pop();
 
             for(int i = 0; i < 4; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
 
-                if(nrow >= 0 && nrow < n && ncol >=0 && ncol < m && grid[nrow][ncol] == 1 && vis[nrow][ncol] != 2){
-                    q.push({{nrow, ncol}, t+1});
+                if(nrow >=0 && nrow < n && ncol >=0 && ncol < m && grid[nrow][ncol] == 1 && vis[nrow][ncol] != 2){
                     vis[nrow][ncol] = 2;
+                    q.push({{nrow, ncol}, t + 1});
                 }
+                time = max(time, t);
             }
         }
-
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(grid[i][j] == 1 && vis[i][j] != 2){
-                    return -1;
-                }
+                if(grid[i][j] == 1 && vis[i][j] != 2) return -1;
             }
         }
         return time;
