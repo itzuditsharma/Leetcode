2149defc2e9b309940044ccc5d76d9238ad9bb26
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-04-02 21:53:56 +0530

    Time: 0 ms (100%), Space: 18.1 MB (59%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int initial_color = image[sr][sc];
-        queue<pair<int, int>> q;
-        q.push({sr, sc});
-        vector<vector<int>> new_image = image;
-        new_image[sr][sc] = color;
-        int n = image.size();
-        int m = image[0].size();
+    void dfs(int row, int col, vector<vector<int>>& image, int ini_color, int color, vector<vector<int>> &new_image, int n, int m, int drow[], int dcol[]){
+        new_image[row][col] = color;
 
-        int drow[]  = {-1, 0, 1, 0};
-        int dcol[] = {0, 1, 0, -1};
+        for(int i = 0; i < 4; i++){
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
 
-        while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-
-            for(int i = 0; i < 4; i++){
-                int nrow = row + drow[i];
-                int ncol = col + dcol[i];
-
-                if(nrow >=0 && nrow < n && ncol >=0 && ncol < m && new_image[nrow][ncol] != color && image[nrow][ncol] == initial_color){
-                    new_image[nrow][ncol] = color;
-                    q.push({nrow, ncol});
-                }
+            if(nrow >=0 && nrow < n && ncol >=0 && ncol < m && image[nrow][ncol] == ini_color && new_image[nrow][ncol] != color){
+                dfs(nrow, ncol, image, ini_color, color, new_image, n, m, drow, dcol);
             }
         }
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int ini_color = image[sr][sc];
+        vector<vector<int>> new_image = image;
+        int n = image.size();
+        int m = image[0].size();
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, 1, 0, -1};
+        dfs(sr, sc, image,ini_color, color, new_image, n, m, drow, dcol);
         return new_image;
     }
 };
