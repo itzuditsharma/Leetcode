241f21662738782b9e43174c38426577d64efa0f
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-04-15 22:34:54 +0530

    Time: 3 ms (81.5%), Space: 20.1 MB (43.64%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n = numCourses;
-        vector<int> indegree(n,0);
-
+        vector<int> indegree(n, 0);
         vector<vector<int>> adj(n);
+        
         for(auto it : prerequisites){
             adj[it[0]].push_back(it[1]);
         }
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i< n; i++){
             for(auto it : adj[i]){
                 indegree[it]++;
             }
         }
-
         queue<int> q;
-        for(int i = 0;i < n; i++){
-            if(indegree[i] == 0) q.push(i);
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
         }
         vector<int> topo;
         while(!q.empty()){
@@ -25,16 +26,18 @@ public:
             q.pop();
             topo.push_back(node);
 
-
             for(auto it : adj[node]){
                 indegree[it]--;
 
-                if(indegree[it] == 0) q.push(it);
+                if(indegree[it] == 0){
+                    q.push(it);
+                }
             }
-
         }
 
-        if(topo.size() == n) return true;
+        if(topo.size() == n){
+            return true;
+        }
         return false;
     }
 };
