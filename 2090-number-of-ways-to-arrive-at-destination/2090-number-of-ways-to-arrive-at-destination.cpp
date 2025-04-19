26cd6ecf7b6ca680da01697ff737473a634d3ffa
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-04-19 16:49:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -2,29 +2,25 @@ class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
         vector<vector<pair<int, int>>> adj(n);
-        // node -> {adjNode, edw}
         for(auto it : roads){
             adj[it[0]].push_back({it[1], it[2]});
             adj[it[1]].push_back({it[0], it[2]});
         }
-
-        // {dist, node}
-        priority_queue<pair<long long, long long>, 
-        vector<pair<long long, long long>>, 
+        int mod = (1e9 + 7);
+        // dis, node 
+        priority_queue<pair<long long, long long>,
+        vector<pair<long long, long long>>,
         greater<pair<long long, long long>>> pq;
-        pq.push({0,0});
 
-        vector<long long int> dist(n, 1e18);
-        dist[0] = 0;
+        pq.push({0,0});
+        vector<long long int> dist(n, 1e9);
         vector<long long int> ways(n, 0);
+        dist[0] = 0;
         ways[0] = 1;
 
-        int mod = (1e9 + 7);
-
         while(!pq.empty()){
-            auto it = pq.top();
-            long long dis = it.first;
-            int node = it.second;
+            long long dis = pq.top().first;
+            int node = pq.top().second;
             pq.pop();
 
             for(auto it : adj[node]){
@@ -36,7 +32,8 @@ public:
                     pq.push({edw + dis, adjNode});
                     ways[adjNode] = ways[node];
                 }else if(dist[adjNode] == edw + dis){
-                    ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
+                    dist[adjNode] = edw + dis;
+                    ways[adjNode] = ways[node] + ways[adjNode];
                 }
             }
         }
