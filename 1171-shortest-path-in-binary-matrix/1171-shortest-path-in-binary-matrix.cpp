d0aa1fa87a9992d3cd86beb9f04044836c6653cd
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-04-19 16:17:10 +0530

    Time: 17 ms (66.37%), Space: 26 MB (52.89%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -3,18 +3,17 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
+        vector<vector<int>> dist(n, vector<int>(m, 1e9));
 
         if(grid[0][0] != 0 || grid[n-1][m-1] != 0) return -1;
 
-        if(grid[0][0] == 0 && n == 1 && m ==1) return 1;
+        if(grid[0][0] == 0 && n == 1 && m == 1) return 1;
 
-        vector<vector<int>> dist(n, vector<int>(m, 1e9));
         dist[0][0] = 0;
-
         queue<pair<int, pair<int, int>>> q;
         q.push({1, {0,0}});
-
-        int drow[] = {-1,0, 1, 0, -1, 1, 1, -1};
+        
+        int drow[] = {-1, 0, 1, 0, 1, -1, 1, -1};
         int dcol[] = {0, 1, 0, -1, 1, 1, -1, -1};
 
         while(!q.empty()){
@@ -24,14 +23,14 @@ public:
             int col = it.second.second;
             q.pop();
 
-            for(int i =0; i < 8; i++){
+            for(int i = 0; i < 8; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
 
-                if(nrow >= 0 && nrow < n && ncol >=0 && ncol<m && grid[nrow][ncol] == 0 && dist[nrow][ncol] > dis + 1){
+                if(nrow >=0 && nrow < n && ncol >=0 && ncol < m && grid[nrow][ncol] == 0 && dist[nrow][ncol] > dis + 1){
+                    q.push({dis+1, {nrow, ncol}});
+                    if(nrow == n-1 && ncol == m-1) return dis+1;
                     dist[nrow][ncol] = dis + 1;
-                    if(nrow == n-1 && ncol == m-1) return dis + 1;
-                    q.push({dis + 1, {nrow, ncol}});
                 }
             }
         }
