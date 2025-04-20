2b5963aa9f8785e5d75cda2e7394d71f5ed91cc0
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-04-20 21:46:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        priority_queue<pair<int, pair<int, int>>, 
-        vector<pair<int, pair<int, int>>>, 
-        greater<pair<int, pair<int, int>>>> pq;
-
-        pq.push({0, {0, 0}});
         int n = heights.size();
         int m = heights[0].size();
+        priority_queue<pair<int, pair<int, int>>,
+        vector<pair<int, pair<int, int>>>,
+        greater<pair<int, pair<int, int>>>> pq;
+        // {diff, {row, col}}
+        pq.push({0, {0,0}});
 
         vector<vector<int>> dist(n, vector<int>(m, 1e9));
         dist[0][0] = 0;
@@ -22,22 +22,21 @@ public:
             int col = it.second.second;
             pq.pop();
 
-            if(row == n-1 && col == m-1) return diff;
-
             for(int i = 0; i < 4; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
 
-                if(nrow >=0 && nrow < n && ncol >= 0 && ncol < m){
-                    int newDiff = max(diff, abs(heights[nrow][ncol] - heights[row][col]));
+                if(nrow >=0 && nrow < n && ncol >=0 && ncol < m){
+                    int newDiff = max(diff, heights[nrow][ncol] - heights[row][col]);
                     if(newDiff < dist[nrow][ncol]){
-                        dist[nrow][ncol] = newDiff;
+                        dist[nrow][ncol]=newDiff;
                         pq.push({newDiff, {nrow, ncol}});
                     }
                 }
             }
-        
         }
-        return -1;
+
+        return dist[n-1][m-1];
+        
     }
 };
