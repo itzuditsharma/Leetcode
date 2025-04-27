7c838b63e1fd38c0874ca42b31aa335b2f3ee852
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-04-27 19:36:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,12 +3,12 @@ public:
     int lengthOfLongestSubstring(string s) {
         int i = 0;
         int j = 0;
+        if(s.size()==0) return 0;
         unordered_map<char, int> mapp;
         int maxx = 1;
-        if(s.size()==0) return 0;
         while(j < s.size()){
-            if(mapp.find(s[j]) != mapp.end() && mapp[s[j]] >= i){
-                i = mapp[s[j]] + 1;
+            if(mapp.find(s[j]) != mapp.end()){
+                i = mapp[s[j]]+1;
             }
 
             mapp[s[j]] = j;
