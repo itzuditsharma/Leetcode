700b6f6ac89e717efd6489bc77b28cea4494dacd
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-04-29 12:21:54 +0530

    Time: 3 ms (74.46%), Space: 10.9 MB (42.99%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int i = 0;
-        int j = 0;
-        int mapp[26] = {0};
+        int l = 0;
+        int r = 0;
         int maxlen = 0;
+        int hash[26] = {0};
         int maxfeq = 0;
+        while(r < s.size()){
+            hash[s[r] - 'A']++;
+            maxfeq = max(maxfeq, hash[s[r] - 'A']);
 
-        while(j < s.size()){
-            mapp[s[j] - 'A']++;
-            maxfeq = max(maxfeq, mapp[s[j] - 'A']);
-            while(((j-i+1) - maxfeq) > k){
-                mapp[s[i] - 'A']--;
+            while((r-l+1) - maxfeq> k){
+                hash[s[l]-'A']--;
+                l++;
                 maxfeq = 0;
                 for(int i = 0; i < 26; i++){
-                    maxfeq = max(maxfeq, mapp[i]);
+                    maxfeq = max(maxfeq, hash[i]);
                 }
-                i++;
             }
 
-            if(((j-i+1) - maxfeq) <= k){
-                maxlen = max(maxlen, j-i+1);
+            if((r-l+1) - maxfeq <= k){
+                maxlen = max(maxlen, r-l+1);
+                r++;
             }
-            j++;
+
         }
         return maxlen;
     }
