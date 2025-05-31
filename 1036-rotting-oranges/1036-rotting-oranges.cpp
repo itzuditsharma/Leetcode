4c7614fe016a723afaf23a3e28d5cba3626f5f4b
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-05-31 17:12:21 +0530

    Time: 1 ms (41.38%), Space: 16.7 MB (88.19%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -5,46 +5,46 @@ public:
         int m = grid[0].size();
         int vis[n][m];
         queue<pair<pair<int, int>, int>> q;
-
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == 2){
-                    q.push({{i, j}, 0});
+                    q.push({{i,j}, 0});
                     vis[i][j] = 2;
                 }else{
                     vis[i][j] = 0;
                 }
             }
         }
-        
+
+        int time = 0;
         int drow[] = {-1, 0, 1, 0};
         int dcol[] = {0, 1, 0, -1};
-        int time = 0;
 
         while(!q.empty()){
-            auto it = q.front();
-            int row = it.first.first;
-            int col = it.first.second;
-            int t = it.second;
-
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int t = q.front().second;
             q.pop();
+            time = max(time, t);
 
             for(int i = 0; i < 4; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
-
-                if(nrow >=0 && nrow < n && ncol >=0 && ncol < m && grid[nrow][ncol] == 1 && vis[nrow][ncol] != 2){
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] != 2 && grid[nrow][ncol] ==1){
+                    q.push({{nrow, ncol}, t+1});
                     vis[nrow][ncol] = 2;
-                    q.push({{nrow, ncol}, t + 1});
                 }
-                time = max(time, t);
             }
         }
+
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(grid[i][j] == 1 && vis[i][j] != 2) return -1;
+                if(vis[i][j] != 2 && grid[i][j] == 1){
+                    return -1;
+                }
             }
         }
+
         return time;
     }
 };
