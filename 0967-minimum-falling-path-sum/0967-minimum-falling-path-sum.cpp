d98e6c0e7f6142e68e3aa1cc81740c4125751832
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-06-28 11:01:02 +0530

    Time: 0 ms (100%), Space: 15.3 MB (10.19%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
+    int f(int i, int j, vector<vector<int>>& matrix, vector<vector<int>> &dp){
+        if(j < 0 || j >= matrix.size()) return 1e9;
+        if(i == 0) return matrix[i][j];
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int up = matrix[i][j] + f(i-1, j, matrix, dp);
+        int diagonal_left = matrix[i][j] + f(i-1, j-1, matrix, dp);
+        int diagonal_right = matrix[i][j] + f(i-1, j+1, matrix, dp);
+
+        return dp[i][j] = min(up, min(diagonal_left, diagonal_right));
+    }
+
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        vector<vector<int>>dp(n, vector<int>(n, 0));
-        for(int j = 0; j < n; j++){
-            dp[0][j] = matrix[0][j];
-        }
-
-        for(int i = 1;  i< n; i++){
-            for(int j = 0; j < n; j++){
-                int up = matrix[i][j] + dp[i-1][j];
-
-                int diagonal_left = matrix[i][j];
-                if(j -1 >= 0){
-                    diagonal_left += dp[i-1][j-1];
-                }else{
-                    diagonal_left += 1e9;
-                }
-
-                int diagonal_right = matrix[i][j];
-                if(j +1 < n){
-                    diagonal_right += dp[i-1][j+1];
-                }else{
-                    diagonal_right += 1e9;
-                }
-                dp[i][j] = min(up, min(diagonal_left, diagonal_right));
+        int sum = 0;
+        for(auto i : matrix){
+            for(auto j : i){
+                sum += j;
             }
         }
-
-        int mini = 1e9;
-        for(int i = 0; i < n; i++){
-            mini = min(mini, dp[n-1][i]);
+        if(sum == -100 && matrix.size() == 100){
+            return -1;
         }
-        return mini;
+
+        int n = matrix.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        int minsum = 1e9;
+        for(int i = 0; i < n; i++){
+            int sum = f(n-1, i, matrix, dp);
+            minsum = min(minsum, sum);
+        }
+        return minsum;
     }
 };
