225faf5bdea0737834abb0d2541ca5a35e737343
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-06-29 09:19:04 +0530

    Time: 155 ms (31.32%), Space: 17.3 MB (69.35%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -23,7 +23,7 @@ public:
 
         vector<vector<bool>> dp(n, vector<bool>(k+1, 0));
         for(int i = 0; i < n; i++) dp[i][0] = true;
-        dp[0][nums[0]] = true;
+        if(nums[0] <= k) dp[0][nums[0]] = true;
 
         for(int ind = 1; ind < n; ind++){
             for(int target = 0; target <= k; target++){
