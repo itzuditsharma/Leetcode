9c19679eff606a991737a5ed43a5eb75f6c413f1
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-06-29 09:18:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    bool f(int ind, int target, vector<int>& nums, vector<vector<int>> &dp){
-        if(target == 0) return true;
-        if(ind == 0) return (nums[0] == target);
+    // bool f(int ind, int target, vector<int>& nums, vector<vector<int>> &dp){
+    //     if(target == 0) return true;
+    //     if(ind == 0) return (nums[0] == target);
 
-        if(dp[ind][target] != -1) return dp[ind][target];
+    //     if(dp[ind][target] != -1) return dp[ind][target];
 
-        int nottake = f(ind - 1, target, nums, dp);
-        int take = false;
-        if(nums[ind] <= target) take = f(ind - 1, target - nums[ind], nums, dp);
+    //     int nottake = f(ind - 1, target, nums, dp);
+    //     int take = false;
+    //     if(nums[ind] <= target) take = f(ind - 1, target - nums[ind], nums, dp);
 
-        return dp[ind][target] = take | nottake;
-    }
+    //     return dp[ind][target] = take | nottake;
+    // }
 
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
@@ -21,7 +21,19 @@ public:
         if(sum % 2 == 1) return false;
         int k = sum / 2;
 
-        vector<vector<int>> dp(n, vector<int>(k+1, -1));
-        return f(n-1, k, nums, dp);
+        vector<vector<bool>> dp(n, vector<bool>(k+1, 0));
+        for(int i = 0; i < n; i++) dp[i][0] = true;
+        dp[0][nums[0]] = true;
+
+        for(int ind = 1; ind < n; ind++){
+            for(int target = 0; target <= k; target++){
+                int nottake = dp[ind - 1][target];
+                int take = false;
+                if(nums[ind] <= target) take = dp[ind - 1][target - nums[ind]];
+
+                dp[ind][target] = take | nottake;
+            }
+        }
+        return dp[n-1][k];
     }
 };
