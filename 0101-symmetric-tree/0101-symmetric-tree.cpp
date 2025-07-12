f2ba714fa9b3ae56e60f13f5fca667ad0dec00c2
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-07-12 18:17:53 +0530

    Time: 0 ms (100%), Space: 18.2 MB (98.17%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -13,18 +13,17 @@ class Solution {
 public:
     bool helper(TreeNode* p, TreeNode* q){
         if(p == NULL && q == NULL) return true;
-
         if(p && q){
-            if(p -> val != q -> val) return false;
+            if(p->val != q->val) return false;
             else{
-                return helper(p-> left, q->right) && helper(p -> right, q -> left);
+                return helper(p->left, q->right) && helper(p-> right, q-> left);
             }
         }
         return false;
     }
 
     bool isSymmetric(TreeNode* root) {
-        if(root== NULL) return true;
-        return helper(root->left, root->right);
+        if(root == NULL) return true;
+        return helper(root->left, root -> right);
     }
 };
