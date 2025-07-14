84dc059d97ce21b28e1de713b07d06cdee7a2e5f
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-07-14 16:59:57 +0530

    Time: 2 ms (12.67%), Space: 12.5 MB (50%) - LeetHub

--- 1764-maximum-repeating-substring/1764-maximum-repeating-substring.py
+++ 1764-maximum-repeating-substring/1764-maximum-repeating-substring.py
@@ -8,15 +8,18 @@ class Solution(object):
         if sequence == word:
             return 1
 
-        i = 0
         max_repeats = 0
+        i = 0
         len_s = len(word)
 
         while i <= len(sequence) - len_s:
             count = 0
             while sequence[i + count * len_s : i + (count + 1) * len_s] == word:
                 count += 1
+
             max_repeats = max(max_repeats, count)
-            i+=1
+            i+= 1
         
         return max_repeats
+
+        
