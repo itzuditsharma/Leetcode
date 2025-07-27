a4702dbdbce4d6b9a77ed1061e909e7c052098a3
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-07-27 07:59:02 +0530

    Time: 0 ms (100%), Space: 17.9 MB (70.16%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,27 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        key = image[sr][sc]
+        image[sr][sc] = color
+        n = len(image)
+        m = len(image[0])
+        visited = [[0 for _ in range(m)] for _ in range(n)]
+        q = deque()
+        q.append((sr, sc))
+
+        drow = [-1, 0, 1, 0]
+        dcol = [0, 1, 0, -1]
+
+        while q:
+            (row, col) = q.popleft()
+            
+            for i in range(4):
+                nrow = row + drow[i]
+                ncol = col + dcol[i]
+
+                if(nrow >=0 and nrow < n and ncol >= 0 and ncol < m and visited[nrow][ncol] == 0 and image[nrow][ncol] == key):
+                    image[nrow][ncol] = color
+                    visited[nrow][ncol] = 1
+                    q.append((nrow, ncol))
+
+        return image
+
