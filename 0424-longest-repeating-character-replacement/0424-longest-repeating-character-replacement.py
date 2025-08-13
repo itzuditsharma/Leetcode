9d98306ff1e528f186674d2e81b9b43a8e92b8ed
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-08-13 16:17:13 +0530

    Time: 557 ms (5%), Space: 18 MB (87.15%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,24 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        n = len(s)
+        i = 0
+        j = 0
+        maxlen = 0
+        maxfeq = 0
+        hash_array = [0 for _ in range(26)]
+
+        while j < n:    
+            hash_array[ord(s[j]) - ord('A')] += 1
+            maxfeq = max(maxfeq, hash_array[ord(s[j]) - ord('A')])
+
+            while (j-i+1) - maxfeq > k:
+                hash_array[ord(s[i]) - ord('A')] -= 1
+                i+=1
+                maxfeq = 0
+                for idx in range(26):
+                    maxfeq = max(maxfeq, hash_array[idx])
+
+            maxlen = max(maxlen, j-i+1)
+            j+=1
+        
+        return maxlen
