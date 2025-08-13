d43f136e93745ccc47d2005c5653e986d898036d
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-08-13 13:42:55 +0530

    Time: 55 ms (14.12%), Space: 17.9 MB (52.5%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,14 +5,14 @@ class Solution:
         j = 0
         mapp = {}
         maxlen = 0
+
         while j < n:
             if s[j] in mapp and mapp[s[j]] >= i:
                 i = mapp[s[j]] + 1
 
             mapp[s[j]] = j
-            maxlen = max(j-i+1, maxlen)
-            j += 1
+            maxlen = max(maxlen, j-i+1)
+            j+=1
         
         return maxlen
 
-                
