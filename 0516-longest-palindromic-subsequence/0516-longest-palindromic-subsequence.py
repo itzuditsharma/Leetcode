72b4e5c534f8e4e05441241d72962a1f3bc477bd
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-08-15 09:34:33 +0530

    Time: 1674 ms (16.78%), Space: 43.6 MB (27%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,23 +1,16 @@
 class Solution:
-    def findLCS(self, i, j, str1, str2, dp):
-        if i < 0 or j < 0:
-            return 0
-
-        if dp[i][j] != -1:
-            return dp[i][j]
-        
-        if str1[i] == str2[j]:
-            return 1 + self.findLCS(i-1, j-1, str1, str2, dp)
-
-        dp[i][j] = max(self.findLCS(i-1, j, str1, str2, dp), self.findLCS(i, j-1, str1, str2, dp))
-        return dp[i][j]
-
     def longestPalindromeSubseq(self, s: str) -> int:
         n = len(s)
         s1 = s
         s2 = s[::-1]
 
-        dp = [[-1 for _ in range(n)] for _ in range(n)]
+        dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
 
-        lcs = self.findLCS(n-1,n-1,s1,s2,dp)
-        return lcs
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if s1[i-1]==s2[j-1]:
+                    dp[i][j] =  1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        return dp[n][n]
