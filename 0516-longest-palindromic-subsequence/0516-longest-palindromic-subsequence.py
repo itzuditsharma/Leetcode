2346a2f93d2ce5a3f5a38f8e7d4017a619123e47
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-08-15 09:20:22 +0530

    Time: 1188 ms (46.12%), Space: 26.5 MB (88.61%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,19 +1,23 @@
 class Solution:
-    def LCS(self, word1, word2, i, j, dp):
+    def findLCS(self, i, j, str1, str2, dp):
         if i < 0 or j < 0:
             return 0
-        
+
         if dp[i][j] != -1:
             return dp[i][j]
-        if word1[i] == word2[j]:
-            return 1 + self.LCS(word1, word2, i-1, j-1, dp)
         
-        dp[i][j] = max(self.LCS(word1, word2, i-1, j, dp), self.LCS(word1, word2, i, j-1, dp))
-        
+        if str1[i] == str2[j]:
+            return 1 + self.findLCS(i-1, j-1, str1, str2, dp)
+
+        dp[i][j] = max(self.findLCS(i-1, j, str1, str2, dp), self.findLCS(i, j-1, str1, str2, dp))
         return dp[i][j]
-        
+
     def longestPalindromeSubseq(self, s: str) -> int:
         n = len(s)
+        s1 = s
         s2 = s[::-1]
-        dp = [[-1 for i in range(n)] for _ in range(n)]
-        return self.LCS(s, s2, n-1, n-1, dp)
+
+        dp = [[-1 for _ in range(n)] for _ in range(n)]
+
+        lcs = self.findLCS(n-1,n-1,s1,s2,dp)
+        return lcs
