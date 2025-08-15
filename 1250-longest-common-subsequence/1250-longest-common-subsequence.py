0af6d1eecc203fcf36d16dd91f66cd6a2d5ec3cb
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-08-15 08:40:18 +0530

    Time: 739 ms (28.02%), Space: 26.8 MB (84.54%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,12 +1,12 @@
 class Solution:
     def lcs(self, i, j, text1, text2, dp):
-        if i == 0 or j == 0:
+        if i < 0 or j < 0:
             return 0
 
         if dp[i][j] != -1:
             return dp[i][j]
         
-        if text1[i-1] == text2[j-1]:
+        if text1[i] == text2[j]:
             return 1 + self.lcs(i-1, j-1, text1, text2, dp)
         
         dp[i][j] = max(self.lcs(i-1, j, text1, text2, dp), self.lcs(i, j-1, text1, text2, dp))
@@ -16,6 +16,6 @@ class Solution:
         n = len(text1)
         m = len(text2)
 
-        dp = [[-1 for _ in range(m+1)] for _ in range(n+1)]
+        dp = [[-1 for _ in range(m)] for _ in range(n)]
 
-        return self.lcs(n, m, text1, text2, dp)
+        return self.lcs(n-1, m-1, text1, text2, dp)
