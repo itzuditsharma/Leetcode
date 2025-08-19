cdd985f69e90e9efebef65b5fa23851aa8c73894
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-08-19 18:24:28 +0530

    Time: 32 ms (23.06%), Space: 20.3 MB (33.08%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,23 +1,21 @@
 class Solution:
-    def mps(self, i, j , grid, dp):
+    def helper(self, i, j , grid, dp):
         if i == 0 and j == 0:
             return grid[i][j]
-        if j < 0 or i < 0:
-            return 1e8
-        
+        if i < 0 or j < 0:
+            return 1e9
+
         if dp[i][j] != -1:
             return dp[i][j]
 
-        top = grid[i][j] + self.mps(i-1, j, grid, dp)
-        left = grid[i][j] + self.mps(i, j-1, grid, dp)
+        up = grid[i][j] +  self.helper(i-1, j, grid, dp)
+        left = grid[i][j] + self.helper(i, j-1, grid, dp)
 
-        dp[i][j] = min(top, left)
+        dp[i][j] = min(up, left)
         return dp[i][j]
-        
-
 
     def minPathSum(self, grid: List[List[int]]) -> int:
         n = len(grid)
         m = len(grid[0])
-        dp = [[-1 for _ in range(m)] for _ in range(n)]
-        return self.mps(n-1, m-1, grid, dp)
+        dp = [[-1] * m for _ in range(n)]
+        return self.helper(n-1, m-1, grid, dp)
