69814c809858dd49c0c8a9a306c642d5b7a90bae
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-08-21 12:15:29 +0530

    Time: 8513 ms (5%), Space: 147.4 MB (5.01%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,22 +1,22 @@
 class Solution:
-    def LIS(self, ind, prev_ind, nums, n, dp):
-        if ind == n:
+    def helper(self, ind, prev, nums, dp):
+        if ind == len(nums):
             return 0
-        
-        if dp[ind][prev_ind + 1] != -1:
-            return dp[ind][prev_ind+1]
 
-        # not_pick 
-        len_ = 0 + self.LIS(ind+1, prev_ind, nums, n, dp)
-        # pick 
-        if prev_ind == -1 or nums[prev_ind] < nums[ind]:
-            len_ = max(len_, 1 + self.LIS(ind+1, ind, nums, n, dp))
+        if dp[ind][prev + 1] != -1:
+            return dp[ind][prev + 1]
         
-        dp[ind][prev_ind + 1] = len_
-        return dp[ind][prev_ind + 1]
+        # not take 
+        len_ = self.helper(ind + 1, prev, nums, dp)
+
+        if prev == -1 or nums[prev] < nums[ind]:
+            len_ = max(len_, 1 + self.helper(ind + 1, ind, nums, dp))
+        
+        dp[ind][prev+1] = len_
+        return dp[ind][prev+1]
+
 
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
         dp = [[-1 for _ in range(n)] for _ in range(n)]
-        return self.LIS(0, -1, nums, n, dp)
-        
+        return self.helper(0, -1, nums, dp)
