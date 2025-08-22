546b367221fcb16fcfb1b15bb009b74bb452c1b9
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-08-22 11:25:52 +0530

    Time: 0 ms (100%), Space: 19.1 MB (23.36%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,21 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        sorted_list = []
-        for i,val in enumerate(nums):
-            sorted_list.append((val, i))
-        
-        sorted_list.sort(key = lambda x : x[0])
+        mapp = {}
 
-        i = 0
-        j = len(nums) - 1
-
-        while i < j:
-            summation = sorted_list[i][0] + sorted_list[j][0]
-            if summation == target:
-                return [sorted_list[i][1], sorted_list[j][1]]
-            elif summation > target:
-                j-=1
-            else:
-                i+=1
-        
+        for i, val in enumerate(nums):
+            remain = target - val
+            if mapp.get(remain) is not None:
+                return [mapp[remain], i]
+            mapp[val] = i
+            
         return []
