624e9d121f5ec37d427737ac82cda2625a40ce61
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-08-22 21:20:08 +0530

    Time: 11 ms (82.19%), Space: 20.7 MB (76.79%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,6 +1,5 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = []
         mapp = {}
 
         for x in strs:
@@ -9,7 +8,4 @@ class Solution:
                 mapp[key] = []
             mapp[key].append(x)
 
-        for key, val in mapp.items():
-            ans.append(val)
-        
-        return ans
+        return list(mapp.values())
