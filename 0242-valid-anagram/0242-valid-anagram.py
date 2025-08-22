c1f7d8192005023f2867e462c5e7386b2b155e59
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-08-22 12:00:19 +0530

    Time: 15 ms (42.29%), Space: 17.7 MB (86.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        if len(s) != len(t):
+            return False
+        mapp = {}
+        for val in s:
+            mapp[val] = mapp.get(val, 0) + 1
+
+        for x in t:
+            if x not in mapp or mapp[x] == 0:
+                return False
+            mapp[x] -= 1
+
+        return True 
