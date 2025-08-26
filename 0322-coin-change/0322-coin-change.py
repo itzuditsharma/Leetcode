738cb614cff8a5170d4f9e9c9828ffa421e1e856
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-08-26 22:14:23 +0530

    Time: 878 ms (34.16%), Space: 19.3 MB (29.98%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,23 +1,24 @@
 class Solution:
-    def helper(self, i, coins, dp, amount):
-        if i == 0:
-            if amount % coins[i] == 0:
-                return amount // coins[i]
-            return 10**9
-        
+    def helper(self, ind, amount, coins,dp):
+        if ind == 0:
+            if amount % coins[ind] == 0:
+                return amount // coins[ind]
+            return 1e9
 
-        if dp[i][amount] != -1:
-            return dp[i][amount]
-        notTake = self.helper(i-1, coins, dp, amount)
+        if dp[ind][amount] != -1:
+            return dp[ind][amount]
+
+        not_take = self.helper(ind - 1, amount, coins, dp)
         take = 1e9
-        if amount >= coins[i]:
-            take = 1 + self.helper(i, coins, dp, amount - coins[i])
-        
-        dp[i][amount] = min(take, notTake)
-        return dp[i][amount]
+        if (amount >= coins[ind]):
+            take = 1  + self.helper(ind, amount - coins[ind], coins, dp)
+
+        dp[ind][amount] = min(take, not_take)
+
+        return dp[ind][amount]
 
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
         dp = [[-1 for _ in range(amount + 1)] for _ in range(n)]
-        ans = self.helper(n-1, coins, dp, amount)
-        return -1 if ans >= 10**9 else ans
+        ans = self.helper(n-1, amount, coins, dp)
+        return -1 if ans >= 1e9 else ans
