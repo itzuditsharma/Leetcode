b4d87489f05f8b965269944bcf70c9068fbc9a6b
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-09-04 16:02:48 +0530

    Time: 1554 ms (21.95%), Space: 0B (100%) - LeetHub

--- 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,10 +1,18 @@
 # Write your MySQL query statement below
--- Visits 
--- Transactions
-
-SELECT v.customer_id, COUNT(customer_id) as count_no_trans
-FROM Visits v
-LEFT JOIN Transactions t
+SELECT v.customer_id, COUNT(v.customer_id) as count_no_trans
+FROM Visits v LEFT JOIN Transactions t 
 ON v.visit_id = t.visit_id
-WHERE t.transaction_id iS NULL
-GROUP BY v.customer_id
+WHERE t.transaction_id is NULL
+GROUP BY customer_id
+
+-- | visit_id | customer_id | transaction_id | visit_id | amount |
+-- | -------- | ----------- | -------------- | -------- | ------ |
+-- | 1        | 23          | 12             | 1        | 910    |
+-- | 2        | 9           | 13             | 2        | 970    |
+-- | 4        | 30          | null           | null     | null   |
+-- | 5        | 54          | 9              | 5        | 200    |
+-- | 5        | 54          | 3              | 5        | 300    |
+-- | 5        | 54          | 2              | 5        | 310    |
+-- | 6        | 96          | null           | null     | null   |
+-- | 7        | 54          | null           | null     | null   |
+-- | 8        | 54          | null           | null     | null   |
