4da02d1921c2f0f49f7a92be8a9b62dd2a1621a5
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-09-07 22:21:53 +0530

    Time: 11 ms (15.24%), Space: 17.9 MB (54.62%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -3,22 +3,22 @@ class Solution:
         mapp1 = {}
         mapp2 = {}
 
-        if len(s) != len(t):
-            return False
-        
-        i = 0
-        j = 0
+        n = len(s)
+        m = len(t)
 
-        while i < len(s) and j < len(t):
+        if n !=m:
+            return False
+        i, j = 0,0
+        while i < n and j < m:
             if s[i] in mapp1 and mapp1[s[i]] != t[j]:
                 return False
             if t[j] in mapp2 and mapp2[t[j]] != s[i]:
                 return False
-            
+
             mapp1[s[i]] = t[j]
             mapp2[t[j]] = s[i]
 
-            i+=1 
+            i+=1
             j+=1
         
         return True
