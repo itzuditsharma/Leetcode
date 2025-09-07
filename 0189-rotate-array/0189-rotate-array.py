90fe289b5779cec986600ccc823b56acc0f0d35f
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-09-07 23:56:14 +0530

    Time: 1 ms (79.39%), Space: 25.6 MB (46.55%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k = k % n
+        nums[:] = nums[::-1]
+        nums[:k] = nums[:k][::-1]
+        nums[k:] = nums[k:][::-1]
+        
+
