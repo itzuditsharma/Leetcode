b6261b349d484216ca70269b3e1604143fc6e3cd
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-09-10 19:39:05 +0530

    Time: 5 ms (61.03%), Space: 21.3 MB (62.15%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,4 +1,5 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         mapp = Counter(nums)
-        return [first for first, second in mapp.most_common(k)]
+        return [key for key, val in mapp.most_common(k)]
+    
