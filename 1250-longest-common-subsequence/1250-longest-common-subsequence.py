672259ada4ed5bf9dceb43ed552d0f8fa6df3287
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-09-10 19:35:47 +0530

    Time: 723 ms (28.7%), Space: 26.7 MB (85.05%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,22 +1,22 @@
 class Solution:
-    def lcs(self, i, j, text1, text2, dp):
+    def helper(self, i, j, text1, text2, dp):
         if i < 0 or j < 0:
             return 0
-
+        
         if dp[i][j] != -1:
             return dp[i][j]
-        
+
         if text1[i] == text2[j]:
-            dp[i][j]  =  1 + self.lcs(i-1, j-1, text1, text2, dp)
+            dp[i][j] = 1 + self.helper(i-1, j-1, text1, text2, dp)
             return dp[i][j]
         
-        dp[i][j] = max(self.lcs(i-1, j, text1, text2, dp), self.lcs(i, j-1, text1, text2, dp))
+        dp[i][j] = max(self.helper(i-1, j, text1, text2, dp), self.helper(i, j-1, text1, text2, dp))
+
         return dp[i][j]
+
 
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n = len(text1)
         m = len(text2)
-
         dp = [[-1 for _ in range(m)] for _ in range(n)]
-
-        return self.lcs(n-1, m-1, text1, text2, dp)
+        return self.helper(n-1, m-1, text1, text2, dp)
