bce0b04a242645b11855ce30db4dae1df028c6a9
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-09-19 12:53:57 +0530

    Time: 27 ms (83.54%), Space: 20.3 MB (92.95%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,11 +1,13 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         mapp = {}
+        sum = 0
         mapp[0] = 1
         total = 0
-        sum = 0
-        for i in nums:
-            sum += i
+
+        for val in nums:
+            sum += val
+
             remain = sum - k
 
             if remain in mapp:
