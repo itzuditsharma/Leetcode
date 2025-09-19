9cff9f740ede4a1c138daff7096aef27066b97dd
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-09-19 15:49:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,10 +1,40 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        mapp = Counter(nums)
+        count1 = 0
+        count2 = 0
+        element1 = -1
+        element2 = -1
         n = len(nums)
+
+        for i in range(n):
+            if count1 == 0 and nums[i] != element2:
+                count1 = 1
+                element1 = nums[i]
+            elif count2 == 0 and nums[i] != element1:
+                count2 = 1
+                element2 = nums[i]
+            elif nums[i] == element1:
+                count1 += 1
+            elif nums[i] == element2:
+                count2 += 1
+            else:
+                count1 -= 1
+                count2 -= 1
+        
+        count1 = 0
+        count2 = 0
+
+        for i in range(n):
+            if nums[i] == element1:
+                count1 += 1
+            if nums[i] == element2:
+                count2 += 1
+
         ans = []
-        for val, freq in mapp.items():
-            if freq > n/3:
-                ans.append(val)
-            
-        return ans
+        mini = (n//3) + 1
+        if count1 >= mini:
+            ans.append(element1)
+        if count2 >= mini:
+            ans.append(element2)
+        
+        return sorted(ans)
