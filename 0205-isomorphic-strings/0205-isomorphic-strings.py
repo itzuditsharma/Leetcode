e3c44f8bd9e88657ac606f7884018835a1943810
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-10-21 13:29:22 +0530

    Time: 15 ms (5.55%), Space: 18 MB (36.17%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -6,9 +6,12 @@ class Solution:
         n = len(s)
         m = len(t)
 
-        if n !=m:
+        if n != m:
             return False
-        i, j = 0,0
+
+        i = 0
+        j = 0
+
         while i < n and j < m:
             if s[i] in mapp1 and mapp1[s[i]] != t[j]:
                 return False
@@ -18,7 +21,7 @@ class Solution:
             mapp1[s[i]] = t[j]
             mapp2[t[j]] = s[i]
 
-            i+=1
-            j+=1
+            i += 1
+            j += 1
         
         return True
