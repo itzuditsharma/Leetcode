0ddb056d5d4d94cce9ab8fac798ddb5c6949dbde
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-10-21 12:10:50 +0530

    Time: 3 ms (69.18%), Space: 17.8 MB (57.84%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.py
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.py
@@ -6,11 +6,11 @@ class Solution:
         for i in range(len(s)):
             if s[i] == '(':
                 if len(stack) != 0:
-                    ans+=(s[i])
+                    ans += s[i]
                 stack.append(s[i])
             else:
                 stack.pop()
                 if len(stack) != 0:
-                    ans+=(s[i])
-
+                    ans += s[i]
+        
         return ans
