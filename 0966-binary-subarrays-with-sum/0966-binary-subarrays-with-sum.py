084d43de1fa008a74618d47217b281b13245a00f
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-10-28 19:06:48 +0530

    Time: 35 ms (34.83%), Space: 21.6 MB (17.56%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
@@ -1,20 +1,16 @@
 class Solution:
-    def helper(self, nums, goal):
-        summ = 0
-        l = 0
-        r = 0
-        count = 0
-        if goal < 0:
-            return 0
-        while r < len(nums):
-            summ += nums[r]
-            while summ > goal:
-                summ -= nums[l]
-                l+=1
-            count += (r-l+1)
-            r+=1
-            
-        return count
-
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        return self.helper(nums, goal) - self.helper(nums, goal - 1)
+        summ = 0
+        mapp = defaultdict(int)
+        mapp[0] = 1 
+        count = 0
+
+        for i in range(len(nums)):
+            summ += nums[i]
+            remain = summ - goal
+            if remain in mapp:
+                count += mapp[remain]
+            
+            mapp[summ] = mapp[summ] + 1
+        
+        return count
