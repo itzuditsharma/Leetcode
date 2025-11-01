e58360e87358417d6c4a5ba5073f8b66de29396b
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-11-01 11:58:48 +0530

    Time: 324 ms (67.83%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,5 +1,19 @@
 # Write your MySQL query statement below
-SELECT e.name FROM Employee e
-INNER JOIN Employee e2 on e.id = e2.managerId
-GROUP BY e.id
-HAVING COUNT(e.id) >=5
+SELECT e1.name FROM Employee e1 
+LEFT JOIN Employee e2
+on e1.id = e2.managerId
+GROUP BY e1.id
+HAVING COUNT(e1.id) >= 5
+
+-- | id  | name  | department | managerId | id   | name  | department | managerId |
+-- | --- | ----- | ---------- | --------- | ---- | ----- | ---------- | --------- |
+-- | 101 | John  | A          | null      | 106  | Ron   | B          | 101       |
+-- | 101 | John  | A          | null      | 105  | Anne  | A          | 101       |
+-- | 101 | John  | A          | null      | 104  | Amy   | A          | 101       |
+-- | 101 | John  | A          | null      | 103  | James | A          | 101       |
+-- | 101 | John  | A          | null      | 102  | Dan   | A          | 101       |
+-- | 102 | Dan   | A          | 101       | null | null  | null       | null      |
+-- | 103 | James | A          | 101       | null | null  | null       | null      |
+-- | 104 | Amy   | A          | 101       | null | null  | null       | null      |
+-- | 105 | Anne  | A          | 101       | null | null  | null       | null      |
+-- | 106 | Ron   | B          | 101       | null | null  | null       | null      |
