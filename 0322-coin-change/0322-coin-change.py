784b39b363344a92197cb778f3196fb769f771a3
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-11-12 22:52:18 +0530

    Time: 882 ms (33.63%), Space: 19.1 MB (30.52%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,5 +1,5 @@
 class Solution:
-    def helper(self, ind, amount, coins,dp):
+    def helper(self, ind, coins, amount, dp):
         if ind == 0:
             if amount % coins[ind] == 0:
                 return amount // coins[ind]
@@ -8,17 +8,16 @@ class Solution:
         if dp[ind][amount] != -1:
             return dp[ind][amount]
 
-        not_take = self.helper(ind - 1, amount, coins, dp)
+        not_take = self.helper(ind - 1, coins, amount, dp)
         take = 1e9
-        if (amount >= coins[ind]):
-            take = 1  + self.helper(ind, amount - coins[ind], coins, dp)
+        if amount >= coins[ind]:
+            take = 1 + self.helper(ind, coins, amount - coins[ind], dp)
 
         dp[ind][amount] = min(take, not_take)
-
         return dp[ind][amount]
 
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
         dp = [[-1 for _ in range(amount + 1)] for _ in range(n)]
-        ans = self.helper(n-1, amount, coins, dp)
+        ans = self.helper(n-1, coins, amount, dp)
         return -1 if ans >= 1e9 else ans
