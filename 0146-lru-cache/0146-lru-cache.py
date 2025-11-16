14cff2a2a86e4bc9798c429deaca697d9d252dab
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-11-16 18:08:49 +0530

    Time: 189 ms (11.87%), Space: 78.3 MB (22.68%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -5,50 +5,54 @@ class LRUCache:
             self.value = value
             self.prev = None
             self.next = None
-    
+
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.mapp = {}
+        self.map = {}    #key -> Node
         self.head = self.Node(-1, -1)
         self.tail = self.Node(-1, -1)
         self.head.next = self.tail
         self.tail.prev = self.head
 
-    def delNode(self,resNode):
-        prevNode = resNode.prev
-        nextNode = resNode.next
+    # {1:2, 2:3}
+
+    def addNode(self, node):
+        currNode = self.head.next
+        node.next = currNode
+        node.prev = self.head
+        currNode.prev = node
+        self.head.next = node
+
+    def delNode(self, node):
+        prevNode = node.prev
+        nextNode = node.next
         prevNode.next = nextNode
         nextNode.prev = prevNode
-    
-    def addNode(self,resNode):
-        currNode = self.head.next
-        resNode.next = currNode
-        resNode.prev = self.head
-        currNode.prev = resNode
-        self.head.next = resNode
 
     def get(self, key: int) -> int:
-        if key in self.mapp.keys():
-            resNode = self.mapp[key]
-            ans = resNode.value
-            self.delNode(resNode)
-            self.addNode(resNode)
-            self.mapp[key] = self.head.next
+        if key in self.map.keys():
+            node = self.map[key]
+            ans = node.value
+            self.delNode(node)
+            self.addNode(node)
+            self.map[key] = self.head.next
             return ans
+
         return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.mapp.keys():
-            resNode = self.mapp[key]
-            del self.mapp[key]
-            self.delNode(resNode)
-        if len(self.mapp) == self.cap:
-            del self.mapp[self.tail.prev.key]
+        if key in self.map.keys():
+            node = self.map[key]
+            self.delNode(node)
+            del self.map[key]
+        
+        if len(self.map) == self.cap:
+            del self.map[self.tail.prev.key]
             self.delNode(self.tail.prev)
+
         newNode = self.Node(key, value)
         self.addNode(newNode)
-        self.mapp[key] = self.head.next
-
+        self.map[key] = self.head.next
 
 
 # Your LRUCache object will be instantiated and called as such:
