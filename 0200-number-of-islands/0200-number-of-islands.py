79452807a62f6b37d45ec1569769df2083dce850
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-11-18 09:29:45 +0530

    Time: 232 ms (86.36%), Space: 20.1 MB (68.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,5 +1,5 @@
 class Solution:
-    def dfs(self, row, col, vis, grid, drow, dcol):
+    def dfs(self, row, col, grid, vis, drow, dcol):
         vis[row][col] = 1
         n = len(grid)
         m = len(grid[0])
@@ -8,21 +8,21 @@ class Solution:
             nrow = row + drow[i]
             ncol = col + dcol[i]
 
-            if nrow >=0 and nrow < n and ncol >= 0 and ncol < m and vis[nrow][ncol] == 0 and grid[nrow][ncol] == '1':
-                self.dfs(nrow, ncol, vis, grid, drow, dcol)
+            if nrow >= 0 and nrow < n and ncol >= 0 and ncol < m and vis[nrow][ncol] == 0 and grid[nrow][ncol] == '1':
+                self.dfs(nrow, ncol, grid, vis, drow, dcol)
 
     def numIslands(self, grid: List[List[str]]) -> int:
         n = len(grid)
         m = len(grid[0])
-        vis = [[0 for _ in range(m)] for _ in range(n)]
         count = 0
+        visited = [[0 for _ in range(m)] for _ in range(n)]
         drow = [-1, 0, 1, 0]
         dcol = [0, 1, 0, -1]
 
         for i in range(n):
             for j in range(m):
-                if vis[i][j] == 0 and grid[i][j] == '1':
+                if grid[i][j] == '1' and visited[i][j] == 0:
                     count += 1
-                    self.dfs(i, j, vis, grid, drow, dcol)
+                    self.dfs(i, j, grid, visited, drow, dcol)
         
         return count
