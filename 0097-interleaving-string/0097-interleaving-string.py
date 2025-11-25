91308db2fdb38cdddb2310d4040ef6ea0746905a
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-11-25 23:57:47 +0530

    Time: 43 ms (66.08%), Space: 17.8 MB (79.03%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -2,13 +2,11 @@ class Solution:
     def helper(self, i, j, k, s1, s2, s3, dp):
         if i == len(s1) and j == len(s2) and k == len(s3):
             return True
-
+        
         if dp[i][j] != -1:
             return dp[i][j]
-
-        x = False
-        y = False
-        if i < len(s1):
+        x, y = False, False
+        if  i < len(s1):
             if s1[i] == s3[k]:
                 x = self.helper(i+1, j, k+1, s1, s2, s3, dp)
         
@@ -24,6 +22,5 @@ class Solution:
             return False
         a = len(s1)
         b = len(s2)
-        i,j,k = 0,0,0
         dp = [[-1 for _ in range(b+1)] for _ in range(a+1)]
-        return self.helper(0,0,0,s1, s2, s3, dp)
+        return self.helper(0,0,0,s1,s2,s3,dp)
