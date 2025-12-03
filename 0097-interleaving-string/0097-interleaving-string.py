4dcbd8cd0cba1c891c59846b5d7904d027848593
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-12-03 18:25:47 +0530

    Time: 43 ms (66.06%), Space: 18.1 MB (45.29%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,26 +1,26 @@
 class Solution:
-    def helper(self, i, j, k, s1, s2, s3, dp):
+    def helper(self, i, j, k , a, b, s1, s2, s3, dp):
         if i == len(s1) and j == len(s2) and k == len(s3):
             return True
         
         if dp[i][j] != -1:
             return dp[i][j]
-        x, y = False, False
-        if  i < len(s1):
-            if s1[i] == s3[k]:
-                x = self.helper(i+1, j, k+1, s1, s2, s3, dp)
         
-        if j < len(s2):
-            if s2[j] == s3[k]:
-                y = self.helper(i, j+1, k+1, s1, s2, s3, dp)
+        x,y = False, False
+        if i < len(s1) and  s1[i] == s3[k]:
+            x = self.helper(i+1, j, k+1, a, b, s1, s2, s3, dp)
+        
+        if j < len(s2) and s2[j] == s3[k]:
+            y = self.helper(i, j+1, k+1, a, b, s1, s2, s3, dp)
         
         dp[i][j] = x or y
         return dp[i][j]
 
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        if len(s1) + len(s2) != len(s3):
-            return False
         a = len(s1)
         b = len(s2)
+        if a + b != len(s3):
+            return False
         dp = [[-1 for _ in range(b+1)] for _ in range(a+1)]
-        return self.helper(0,0,0,s1,s2,s3,dp)
+        return self.helper(0,0,0,a,b,s1,s2,s3,dp)
+
