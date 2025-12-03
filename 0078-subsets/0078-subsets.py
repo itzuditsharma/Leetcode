53c3be52c9c5cc8148ae9bd485ec3d1333bbdd7e
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-12-03 18:13:35 +0530

    Time: 0 ms (100%), Space: 18.1 MB (20.75%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,17 +1,16 @@
 class Solution:
-    def helper(self, ind, nums, n, ds, ans):
-        if ind == n:
+    def helper(self, ind, nums, ds, ans):
+        if ind == len(nums):
             ans.append(ds[:])
-            return 
-        
-        ds.append(nums[ind])
-        self.helper(ind + 1, nums, n, ds, ans)
-        ds.pop()
-        self.helper(ind + 1, nums, n, ds, ans)
+            return
 
+        ds.append(nums[ind])
+        self.helper(ind + 1, nums, ds, ans)
+        ds.pop()
+        self.helper(ind + 1, nums, ds, ans)
+        
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ds = []
         ans = []
-        n = len(nums)
-        self.helper(0, nums, n, ds, ans)
+        ds = []
+        self.helper(0, nums, ds, ans)
         return ans
