459e61f1ef355f6ec372411e197378a6e9232b2f
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2025-12-18 19:53:58 +0530

    Time: 0 ms (100%), Space: 18.7 MB (66.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,11 @@
+# from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mapp = {}
+        mapp = defaultdict(int)
         for i, val in enumerate(nums):
             remain = target - val
             if remain in mapp:
                 return [mapp[remain], i]
             mapp[val] = i
+        
         return []
