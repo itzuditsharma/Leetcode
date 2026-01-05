3f67596dc2b9b776f508ff406248d07825cc91fe
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2026-01-05 23:33:04 +0530

    Time: 127 ms (69.12%), Space: 33.8 MB (37.97%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,30 +1,17 @@
 class Solution:
-    def helper(self, i, j, matrix, dp):
-        # out of bounds
-        if i < 0 or j < 0:
-            return 0
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        n = len(matrix)
+        m = len(matrix[0])
+        dp = [[0] * m for _ in range(n)]
+        maxside = 0
 
-        if dp[i][j] != -1:
-            return dp[i][j]
-
-        left = self.helper(i, j - 1, matrix, dp)
-        top = self.helper(i - 1, j, matrix, dp)
-        diag = self.helper(i - 1, j - 1, matrix, dp)
-
-        if matrix[i][j] == "1":
-            dp[i][j] = 1 + min(left, top, diag)
-            self.maxi = max(self.maxi, dp[i][j])
-        else:
-            dp[i][j] = 0
-
-        return dp[i][j]
-
-    def maximalSquare(self, matrix):
-        n, m = len(matrix), len(matrix[0])
-        dp = [[-1] * m for _ in range(n)]
-        self.maxi = 0
-
-        # single recursive call
-        self.helper(n - 1, m - 1, matrix, dp)
-
-        return self.maxi * self.maxi
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == "1":
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+                    maxside = max(maxside, dp[i][j])
+        
+        return maxside * maxside
