858f3eaac3d0ed0dbb60d086090b3c748dcd8438
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2026-01-17 11:32:27 +0530

    Time: 235 ms (21.84%), Space: 33.3 MB (44.15%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,17 +1,25 @@
 class Solution:
-    def maximalSquare(self, matrix: List[List[str]]) -> int:
-        n = len(matrix)
-        m = len(matrix[0])
-        dp = [[0] * m for _ in range(n)]
-        maxside = 0
-
-        for i in range(n):
-            for j in range(m):
-                if matrix[i][j] == "1":
-                    if i == 0 or j == 0:
-                        dp[i][j] = 1
-                    else:
-                        dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
-                    maxside = max(maxside, dp[i][j])
+    def helper(self, i, j, dp, matrix):
+        if i < 0 or j < 0:
+            return 0
+        if dp[i][j] != -1:
+            return dp[i][j]
         
-        return maxside * maxside
+        left = self.helper(i, j-1, dp, matrix)
+        top = self.helper(i-1, j , dp, matrix)
+        diag = self.helper(i-1, j-1, dp, matrix)
+
+        if matrix[i][j] == "1":
+            dp[i][j] = 1 + min(left, top, diag)
+            self.maxi = max(self.maxi, dp[i][j])
+        else:
+            dp[i][j] = 0
+        
+        return dp[i][j]
+
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        n, m = len(matrix), len(matrix[0])
+        dp = [[-1] * m for _ in range(n)]
+        self.maxi = 0
+        self.helper(n-1, m-1, dp, matrix)
+        return self.maxi*self.maxi
