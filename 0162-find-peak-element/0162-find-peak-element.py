412b88ce5f3cc31580474e7f5b7aa3ae7d027ce9
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2026-01-24 23:06:26 +0530

    Time: 0 ms (100%), Space: 19.4 MB (14.32%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -8,16 +8,16 @@ class Solution:
             return 0
         if nums[n-1] > nums[n-2]:
             return n-1
-
-        low = 1
-        high = n-2
-        while low <= high:
-            mid = (low + high) // 2
-            if nums[mid - 1] < nums[mid] > nums[mid + 1]:
+        
+        l = 1
+        r = n-2
+        while l <= r:
+            mid = (l+r)//2
+            if nums[mid-1] < nums[mid] > nums[mid+1]:
                 return mid
             if nums[mid] < nums[mid+1]:
-                low = mid + 1
+                l = mid + 1
             else:
-                high = mid - 1
-        
-        return -1
+                r = mid  - 1
+
+        return -1 
