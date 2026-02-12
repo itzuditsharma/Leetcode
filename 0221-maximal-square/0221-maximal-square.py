9cf48b727a552b643b2b23ca35ca87ba34262eaa
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2026-02-12 18:36:14 +0530

    Time: 256 ms (19.54%), Space: 33.2 MB (66.91%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,16 +1,17 @@
 class Solution:
-    def helper(self, i, j, dp, matrix):
+    def helper(self, i, j, matrix, dp):
         if i < 0 or j < 0:
             return 0
+        
         if dp[i][j] != -1:
             return dp[i][j]
         
-        left = self.helper(i, j-1, dp, matrix)
-        top = self.helper(i-1, j , dp, matrix)
-        diag = self.helper(i-1, j-1, dp, matrix)
+        left = self.helper(i, j-1, matrix, dp)
+        diag = self.helper(i-1, j-1, matrix, dp)
+        top = self.helper(i-1, j, matrix, dp)
 
         if matrix[i][j] == "1":
-            dp[i][j] = 1 + min(left, top, diag)
+            dp[i][j] = 1 + min(left, diag, top)
             self.maxi = max(self.maxi, dp[i][j])
         else:
             dp[i][j] = 0
@@ -18,8 +19,10 @@ class Solution:
         return dp[i][j]
 
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        n, m = len(matrix), len(matrix[0])
+        n = len(matrix)
+        m = len(matrix[0])
         dp = [[-1] * m for _ in range(n)]
         self.maxi = 0
-        self.helper(n-1, m-1, dp, matrix)
-        return self.maxi*self.maxi
+
+        self.helper(n-1, m-1, matrix, dp)
+        return self.maxi * self.maxi
