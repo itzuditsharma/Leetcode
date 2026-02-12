c113a06fb015c41894eaad2117daa22add528b19
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2026-02-12 19:16:16 +0530

    Time: 7 ms (42.65%), Space: 20.6 MB (16.96%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,9 +1,8 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        i = j = 0
-        while j < len(nums):
+        i=j=0
+        for j in range(len(nums)):
             if nums[j] != 0:
                 nums[i], nums[j] = nums[j], nums[i]
                 i+=1
             j+=1
-        
