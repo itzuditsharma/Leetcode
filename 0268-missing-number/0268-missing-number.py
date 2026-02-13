3ccbaa24c75faa51dfb9a66ea413be496bb0dced
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2026-02-14 00:10:35 +0530

    Time: 7 ms (36.74%), Space: 20.4 MB (49.49%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -5,7 +5,7 @@ class Solution:
 
         for i in range(len(nums) + 1):
             if i < len(nums):
-                xor_curr += nums[i]
-            xor_actual += i
+                xor_curr ^= nums[i]
+            xor_actual ^= i
         
         return xor_curr ^ xor_actual
