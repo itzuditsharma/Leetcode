17cd768aa12d9370e24c6b09a21073e4d4b0ab07
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2026-02-15 13:13:19 +0530

    Time: 3 ms (52.47%), Space: 20.8 MB (5.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,23 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mapp = defaultdict(int)
+        sorted_array = []
         for i, val in enumerate(nums):
-            remain = target - val
-            if remain in mapp:
-                return [mapp[remain], i]
+            sorted_array.append((val, i))
+        sorted_array.sort()
 
-            mapp[val] = i
+        n = len(nums)
+        left = 0
+        right = n-1
+
+        while left < right:
+            summ = sorted_array[left][0] + sorted_array[right][0]
+            if summ == target:
+                return [sorted_array[left][1], sorted_array[right][1]]
+
+            if summ > target:
+                right -= 1
+            else:
+                left += 1
         
         return []
+            
