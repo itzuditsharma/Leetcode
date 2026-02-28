415b433754ad00b572ba9fe7c4b3a8d47d6348e2
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2026-02-28 15:35:45 +0530

    Time: 0 ms (100%), Space: 19.4 MB (74.04%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -4,23 +4,23 @@ class Solution:
         image[sr][sc] = color
         n = len(image)
         m = len(image[0])
-        vis = [[0 for _ in range(m)] for _ in range(n)]
+        vis = [[0] * m for _ in range(n)]
         vis[sr][sc] = 1
-        q = deque()
-        q.append((sr, sc))
         drow = [-1, 0, 1, 0]
         dcol = [0, 1, 0, -1]
+        q = deque()
+        q.append((sr, sc))
 
         while q:
             (row, col) = q.popleft()
-            
+
             for i in range(4):
                 nrow = row + drow[i]
                 ncol = col + dcol[i]
 
-                if nrow >=0 and nrow < n and ncol >= 0 and ncol < m and vis[nrow][ncol] == 0 and image[nrow][ncol] == key:
-                    image[nrow][ncol] = color
+                if nrow >= 0 and nrow < n and ncol >= 0 and ncol < m and image[nrow][ncol] == key and vis[nrow][ncol] == 0:
                     vis[nrow][ncol] = 1
+                    image[nrow][ncol] = color
                     q.append((nrow, ncol))
-
+        
         return image
