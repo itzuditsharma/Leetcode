b4cd30816b30c78a5b120bbf5583d5af8833c166
itzuditsharma <67915530+itzuditsharma@users.noreply.github.com>
2026-02-28 13:40:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -3,31 +3,31 @@ class Solution:
         n = len(grid)
         m = len(grid[0])
         q = deque()
+        time = 0
 
-        vis = [[0 for _ in range(m)] for _ in range(n)]
+        vis = [[0] * m for _ in range(n)]
 
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 2:
-                    q.append(((i,j), 0))
                     vis[i][j] = 2
-                
-        time = 0
+                    q.append(((i,j), 0))
+        
         drow = [-1, 0, 1, 0]
         dcol = [0, 1, 0, -1]
 
         while q:
-            (row, col), t = q.popleft()
+            (row, col), t = q.pop()
             time = max(time, t)
 
             for i in range(4):
                 nrow = row + drow[i]
                 ncol = col + dcol[i]
 
-                if nrow >=0 and nrow < n and ncol>=0 and ncol < m and vis[nrow][ncol] != 2 and grid[nrow][ncol] == 1:
-                    q.append(((nrow, ncol), t+1))
+                if nrow >= 0 and nrow < n and ncol >=0 and ncol < m and vis[nrow][ncol] == 0 and grid[nrow][ncol] == 1:
                     vis[nrow][ncol] = 2
-        
+                    q.append(((nrow, ncol), t+1))
+
         for i in range(n):
             for j in range(m):
                 if vis[i][j] != 2 and grid[i][j] == 1:
